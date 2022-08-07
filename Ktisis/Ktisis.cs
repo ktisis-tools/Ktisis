670e5c918ec294cd4217f37eb50f3eeada1cd1cb
chirp <ch1rp@protonmail.com>
2022-08-07 22:47:14 +0100

    base for overlay rendering

--- Ktisis/Ktisis.cs
+++ Ktisis/Ktisis.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Numerics;
 
 using Dalamud;
 using Dalamud.IoC;
 using Dalamud.Plugin;
+using Dalamud.Logging;
+using Dalamud.Interface;
+using Dalamud.Game.Gui;
 using Dalamud.Game.Command;
+using Dalamud.Game.ClientState;
+
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
+
+using ImGuiNET;
+
+using Ktisis.Overlay;
 
 namespace Ktisis {
 	public sealed class Ktisis : IDalamudPlugin {
@@ -13,17 +24,49 @@ namespace Ktisis {
 
 		private DalamudPluginInterface PluginInterface { get; init; }
 		private CommandManager CommandManager { get; init; }
+		private ClientState ClientState { get; init; }
+
+		private Skeleton SkeletonOverlay { get; init; }
 
 		public Ktisis(
-			[RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-			[RequiredVersion("1.0")] CommandManager cmdManager
+			DalamudPluginInterface pluginInterface,
+			CommandManager cmdManager,
+			ClientState clientState,
+			GameGui gameGui
 		) {
-			this.PluginInterface = pluginInterface;
-			this.CommandManager = cmdManager;
+			PluginInterface = pluginInterface;
+			CommandManager = cmdManager;
+			ClientState = clientState;
+
+			SkeletonOverlay = new Skeleton(gameGui, null);
+
+			pluginInterface.UiBuilder.Draw += Draw;
 		}
 
 		public void Dispose() {
+			// TODO
+		}
 
+		public unsafe void Draw() {
+			var actor = ClientState.LocalPlayer;
+			if (actor == null)
+				return;
+
+			ImGuiHelpers.ForceNextWindowMainViewport();
+			ImGuiHelpers.SetNextWindowPosRelativeMainViewport(new Vector2(0, 0));
+
+			ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Vector2(0, 0));
+
+			ImGui.Begin("Ktisis Overlay", ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoInputs);
+			ImGui.SetWindowSize(ImGui.GetIO().DisplaySize);
+
+			SkeletonOverlay.Subject = actor;
+			SkeletonOverlay.Draw();
+
+			var draw = ImGui.GetWindowDrawList();
+
+			ImGui.End();
+			ImGui.PopStyleVar();
 		}
 	}
 }
---
+++ Ktisis/Overlay/Skeleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Dalamud.Game.Gui;
+using Dalamud.Game.ClientState.Objects.Types;
+
+using ImGuiNET;
+
+namespace Ktisis.Overlay {
+	public sealed class Skeleton {
+		public GameGui Gui;
+
+		public GameObject? Subject;
+
+		public Skeleton(GameGui gui, GameObject? subject) {
+			Gui = gui;
+			Subject = subject;
+		}
+
+		public void Draw() {
+			if (Subject == null)
+				return;
+
+			if (!Gui.WorldToScreen(Subject.Position, out var pos))
+				return;
+		}
+	}
+}
