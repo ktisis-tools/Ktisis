414fe82f7b9e14aee3cdaaa3792a5360570cffc0
chirp <ch1rp@protonmail.com>
2022-08-21 18:19:51 +0100

    Implement redraw

--- Ktisis/Interface/CustomizeUI.cs
+++ Ktisis/Interface/CustomizeUI.cs
@@ -47,6 +47,13 @@ namespace Ktisis.Interface {
 			SetTarget((Actor*)actor.Address);
 		}
 
+		// Apply customize
+
+		public void Apply(Customize custard) {
+			Target->Customize = custard;
+			Target->Redraw();
+		}
+
 		// Draw window
 
 		public void Draw() {
@@ -69,20 +76,21 @@ namespace Ktisis.Interface {
 
 				// Customize
 
-				var custard = Target->Customize;
+				var custom = Target->Customize;
 
 				// Race
 
-				var curRace = Plugin.Locale.GetString($"{custard.Race}");
+				var curRace = Plugin.Locale.GetString($"{custom.Race}");
 				if (ImGui.BeginCombo("Race", curRace)) {
 					foreach (Race race in Enum.GetValues(typeof(Race))) {
 						var raceName = Plugin.Locale.GetString($"{race}");
-						if (ImGui.Selectable(raceName, race == custard.Race)) {
-							custard.Race = race;
-							custard.Tribe = (Tribe)(
+						if (ImGui.Selectable(raceName, race == custom.Race)) {
+							custom.Race = race;
+							custom.Tribe = (Tribe)(
 								Customize.GetRaceTribeIndex(race)
-								+ 1 - (byte)custard.Tribe % 2
+								+ 1 - (byte)custom.Tribe % 2
 							);
+							Apply(custom);
 						}
 					}
 
@@ -90,13 +98,17 @@ namespace Ktisis.Interface {
 					ImGui.EndCombo();
 				}
 
-				var curTribe = Plugin.Locale.GetString($"{custard.Tribe}");
+				// Tribe
+
+				var curTribe = Plugin.Locale.GetString($"{custom.Tribe}");
 				if (ImGui.BeginCombo("Tribe", curTribe)) {
 					var tribes = Enum.GetValues(typeof(Tribe));
 					for (int i = 0; i < 2; i++) {
-						var tribe = (Tribe)(Customize.GetRaceTribeIndex(custard.Race) + i);
-						if (ImGui.Selectable(Plugin.Locale.GetString($"{tribe}"), tribe == custard.Tribe))
-							custard.Tribe = tribe;
+						var tribe = (Tribe)(Customize.GetRaceTribeIndex(custom.Race) + i);
+						if (ImGui.Selectable(Plugin.Locale.GetString($"{tribe}"), tribe == custom.Tribe)) {
+							custom.Tribe = tribe;
+							Apply(custom);
+						}
 					}
 
 					ImGui.SetItemDefaultFocus();
@@ -104,8 +116,6 @@ namespace Ktisis.Interface {
 				}
 
 				// End
-
-				Target->Customize = custard;
 
 				ImGui.PopStyleVar(1);
 				ImGui.End();
--- Ktisis/Structs/Actor/Actor.cs
+++ Ktisis/Structs/Actor/Actor.cs
@@ -15,5 +15,21 @@ namespace Ktisis.Structs.Actor {
 			get => Marshal.PtrToStringAnsi((IntPtr)GameObject.GetName());
 			set => throw new NotImplementedException();
 		}
+
+		// https://github.com/xivdev/Penumbra/blob/master/Penumbra/Interop/ObjectReloader.cs
+
+		public static void DisableDraw(IntPtr addr)
+		=> ((delegate* unmanaged<IntPtr, void>**)addr)[0][17](addr);
+
+		public static void EnableDraw(IntPtr addr)
+		=> ((delegate* unmanaged<IntPtr, void>**)addr)[0][16](addr);
+
+		public unsafe void Redraw() {
+			fixed (Actor* self = &this) {
+				var ptr = (IntPtr)self;
+				DisableDraw(ptr);
+				EnableDraw(ptr);
+			}
+		}
 	}
 }
