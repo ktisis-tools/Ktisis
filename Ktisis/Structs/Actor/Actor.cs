ca360ec727591ac15b9078f2b06b449358399ef3
chirp <ch1rp@protonmail.com>
2022-08-21 13:38:35 +0100

    Add Customize

--- Ktisis/Structs/Actor/Actor.cs
+++ Ktisis/Structs/Actor/Actor.cs
@@ -3,10 +3,11 @@
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
 
 namespace Ktisis.Structs.Actor {
-	[StructLayout(LayoutKind.Explicit, Size = 0xF8)]
+	[StructLayout(LayoutKind.Explicit, Size = 0x84A)]
 	public unsafe struct Actor {
 		[FieldOffset(0)] public GameObject GameObject;
 
-		[FieldOffset(0xF0)] public ActorModel* Model;
+		[FieldOffset(0x0F0)] public ActorModel* Model;
+		[FieldOffset(0x830)] public Customize Customize;
 	}
 }
