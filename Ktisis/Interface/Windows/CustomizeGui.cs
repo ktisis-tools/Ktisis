01ce2f02ddf471aa8216ba32069a38d020ce3dc9
chirp <ch1rp@protonmail.com>
2022-09-18 01:36:57 +0100

    move remaining function from CustomizeUtil to CustomizeGui

--- Ktisis/Interface/Windows/CustomizeGui.cs
+++ Ktisis/Interface/Windows/CustomizeGui.cs
@@ -3,18 +3,29 @@ using System.Numerics;
 using System.Collections.Generic;
 
 using ImGuiNET;
+using ImGuiScene;
 
 using Dalamud.Interface;
 using Dalamud.Interface.Components;
-using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Game.ClientState.Objects.Enums;
 
-using Ktisis.Util;
+using Ktisis.GameData;
 using Ktisis.Localization;
 using Ktisis.GameData.Excel;
 using Ktisis.Structs.Actor;
 
 namespace Ktisis.Interface.Windows {
+	public struct MenuOption {
+		public Menu Option;
+		public Menu? Color = null;
+
+		public Dictionary<uint, TextureWrap>? Select = null;
+
+		public MenuOption(Menu option) {
+			Option = option;
+		}
+	}
+
 	internal class CustomizeGui {
 		// Constants
 
@@ -76,7 +87,7 @@ namespace Ktisis.Interface.Windows {
 
 				var index = custom.GetMakeIndex();
 				if (index != CustomIndex) {
-					MenuOptions = CustomizeUtil.GetMenuOptions(index);
+					MenuOptions = GetMenuOptions(index);
 					CustomIndex = index;
 				}
 
@@ -247,6 +258,64 @@ namespace Ktisis.Interface.Windows {
 				ImGui.PopStyleVar(1);
 				ImGui.End();
 			}
+		}
+
+		// Build menu options
+
+		public static Dictionary<MenuType, List<MenuOption>> GetMenuOptions(uint index) {
+			var options = new Dictionary<MenuType, List<MenuOption>>();
+
+			var data = Sheets.GetSheet<CharaMakeType>().GetRow(index);
+
+			if (data != null) {
+				for (int i = 0; i < CharaMakeType.MenuCt; i++) {
+					var val = data.Menus[i];
+
+					if (val.Index == 0)
+						break;
+
+					if (val.Index == CustomizeIndex.EyeColor2)
+						continue; // TODO: Heterochromia
+
+					var type = val.Type;
+					if (type == MenuType.Unknown1)
+						type = MenuType.Color;
+					if (type == MenuType.Color)
+						continue;
+
+					if (!options.ContainsKey(type))
+						options[type] = new();
+
+					var opt = new MenuOption(val);
+
+					var next = data.Menus[i + 1];
+					if (next.Type == MenuType.Color)
+						opt.Color = next;
+
+					if (val.HasIcon) {
+						var icons = new Dictionary<uint, TextureWrap>();
+						if (val.IsFeature) {
+							foreach (var row in val.Features) {
+								var feat = row.Value!;
+								var icon = Dalamud.DataManager.GetImGuiTextureHqIcon(feat.Icon);
+								if (feat.FeatureId == 0)
+									continue;
+								icons.Add(feat.FeatureId, icon!);
+							}
+						} else {
+							for (var x = 0; x < val.Count; x++) {
+								var icon = Dalamud.DataManager.GetImGuiTextureHqIcon(val.Params[x]);
+								icons.Add(val.Graphics[x], icon!);
+							}
+						}
+						opt.Select = icons;
+					}
+
+					options[type].Add(opt);
+				}
+			}
+
+			return options;
 		}
 	}
 }
