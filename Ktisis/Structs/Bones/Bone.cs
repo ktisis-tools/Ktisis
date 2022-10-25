ce8a095ee4b63af67816f05f87bbdd402965077e
chirp <ch1rp@protonmail.com>
2022-10-25 22:41:15 +0100

    Bone.GetDescendants

--- Ktisis/Structs/Bones/Bone.cs
+++ Ktisis/Structs/Bones/Bone.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 using FFXIVClientStructs.Havok;
 using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
@@ -28,10 +29,24 @@ namespace Ktisis.Structs.Bones {
 			set => Pose->ModelPose.Data[Index] = value;
 		}
 
+		public Category Category => Category.GetForBone(HkaBone.Name.String);
+
 		public unsafe hkQsTransformf* AccessModelSpace(PropagateOrNot propagate) => Pose->AccessBoneModelSpace(Index, propagate);
 
 		public unsafe Vector3 GetWorldPos(ActorModel* model) => model->Position + Transform.Translation.Rotate(model->Rotation) * model->Height;
 
-		public Category Category => Category.GetForBone(HkaBone.Name.String);
+		public unsafe short[] GetDescendents() {
+			var size = Pose->Skeleton->Bones.Length;
+			var result = new short[size];
+			fixed (short* ptr = &result[0]) {
+				var array = new hkArray<short>() {
+					CapacityAndFlags = size,
+					Data = ptr
+				};
+				Interop.PoseHooks.GetDescendentsFunc(Pose->Skeleton, (short)Index, &array, false);
+				Array.Resize(ref result, array.Length);
+			}
+			return result;
+		}
 	}
 }
