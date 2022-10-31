66be5bc19f3bfa7578ee9e131fb0d939ec48898f
Emyka <emyka4@outlook.com>
2022-10-31 15:00:45 +0000

    Added Icon related and added TextRight Gui Helpers

--- Ktisis/Util/GuiHelpers.cs
+++ Ktisis/Util/GuiHelpers.cs
@@ -31,6 +31,34 @@ namespace Ktisis.Util
 			return accepting;
 		}
 
+		public static void Icon(FontAwesomeIcon icon, bool enabled = true, Vector4? color = null) {
+			string iconText = icon.ToIconString() ?? "";
+			int num = 0;
+			if (color.HasValue) {
+				ImGui.PushStyleColor(ImGuiCol.Button, color.Value);
+				num++;
+			}
+
+			ImGui.PushFont(UiBuilder.IconFont);
+			if (enabled) ImGui.Text(iconText);
+			else ImGui.TextDisabled(iconText);
+			ImGui.PopFont();
+			if (num > 0) {
+				ImGui.PopStyleColor(num);
+			}
+		}
+		public static Vector2 CalcIconSize(FontAwesomeIcon icon) {
+			ImGui.PushFont(UiBuilder.IconFont);
+			var size = ImGui.CalcTextSize(icon.ToIconString());
+			ImGui.PopFont();
+			return size;
+		}
+		public static void IconTooltip(FontAwesomeIcon icon, string tooltip, bool enabled = true, Vector4? color = null) {
+			Icon(icon, enabled, color);
+			Tooltip(tooltip);
+		}
+
+
 		public static void Tooltip(string text) {
 			if (ImGui.IsItemHovered()) {
 				ImGui.BeginTooltip();
@@ -58,6 +86,11 @@ namespace Ktisis.Util
 			return show;
 		}
 
+		public static void TextRight(string text, float offset = 0) {
+			offset = ImGui.GetContentRegionAvail().X - offset - ImGui.CalcTextSize(text).X;
+			ImGui.SetCursorPosX(ImGui.GetCursorPosX() + offset);
+			ImGui.TextUnformatted(text);
+		}
 		public static void TextCentered(string text) {
 			var windowWidth = ImGui.GetWindowSize().X;
 			var textWidth = ImGui.CalcTextSize(text).X;
