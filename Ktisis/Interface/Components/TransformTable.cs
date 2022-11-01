ktisis-tools/Ktisis

commit 82858d6efb9006438e842326e9386b75172c34c8

Removed duplicate method to draw the table, changed labels for the same icons used to change gizmo operation. Transform table and Animation Control now take the available width instead of a fixed width

Ktisis/Interface/Components/TransformTable.cs
@@ -1,12 +1,15 @@
 ﻿using System.Numerics;
+using System.Linq;
 using ImGuiNET;
 
+using Dalamud.Interface;
 using FFXIVClientStructs.Havok;
 
 using Ktisis.Helpers;
 using Ktisis.Overlay;
 using Ktisis.Structs;
 using Ktisis.Structs.Bones;
+using Ktisis.Util;
 
 namespace Ktisis.Interface.Components {
 	// Thanks to Emyka for the original code:
@@ -31,37 +34,45 @@ namespace Ktisis.Interface.Components {
 
 		public bool DrawTable() {
 			var result = false;
-			result |= ImGui.DragFloat3("Position", ref Position, 0.0005f);
-			result |= ImGui.DragFloat3("Rotation", ref Rotation, 0.1f);
-			result |= ImGui.DragFloat3("Scale", ref Scale, 0.01f);
+
+			var iconPos = FontAwesomeIcon.LocationArrow;
+			var iconRot = FontAwesomeIcon.Sync;
+			var iconSca = FontAwesomeIcon.ExpandAlt;
+
+			// Attempt to find the exact size for any font and font size.
+			float[] sizes = new float[3];
+			sizes[0] = GuiHelpers.CalcIconSize(iconPos).X;
+			sizes[1] = GuiHelpers.CalcIconSize(iconRot).X;
+			sizes[2] = GuiHelpers.CalcIconSize(iconSca).X;
+			var rightOffset = sizes.Max() + (ImGui.GetStyle().WindowPadding.X * .75f) + 0.1f;
+			
+
+			ImGui.PushItemWidth(ImGui.GetContentRegionAvail().X - rightOffset);
+			result |= ImGui.DragFloat3("##Position", ref Position, 0.0005f);
+			ImGui.SameLine();
+			GuiHelpers.IconTooltip(iconPos, "Position", true);
+			result |= ImGui.DragFloat3("##Rotation", ref Rotation, 0.1f);
+			ImGui.SameLine();
+			GuiHelpers.IconTooltip(iconRot, "Rotation", true);
+			result |= ImGui.DragFloat3("##Scale", ref Scale, 0.01f);
+			ImGui.SameLine();
+			GuiHelpers.IconTooltip(iconSca, "Scale", true);
+			ImGui.PopItemWidth();
 			IsEditing = result;
 			return result;
 		}
-
-		public bool Draw(Bone bone)
-		{
+		public bool Draw(Bone bone) {
 			var result = false;
 			var gizmo = OverlayWindow.GetGizmo(bone.UniqueName);
-			if (gizmo != null)
-			{
+			if (gizmo != null) {
 				(var savedPosition, var savedRotation, var savedScale) = gizmo.Decompose();
-				(var position, var rotation, var scale) = (savedPosition, savedRotation, savedScale);
-				if (Draw(ref position, ref rotation, ref scale))
-				{
-					(var deltaPosition, var deltaRotation, var deltaScale) = (savedPosition - position, savedRotation - rotation, savedScale - scale);
+				(Position, Rotation, Scale) = (savedPosition, savedRotation, savedScale);
+				if (DrawTable()) {
+					(var deltaPosition, var deltaRotation, var deltaScale) = (savedPosition - Position, savedRotation - Rotation, savedScale - Scale);
 					gizmo.InsertEulerDeltaMatrix(deltaPosition, deltaRotation, deltaScale);
 					result = true;
 				}
 			}
-			IsEditing = result;
-			return result;
-		}
-		public bool Draw(ref Vector3 position, ref Vector3 rotation, ref Vector3 scale)
-		{
-			var result = false;
-			result |= ImGui.DragFloat3("Position", ref position, 0.0005f, -10000f, 10000f, "%.5f");
-			result |= ImGui.DragFloat3("Rotation", ref rotation, 0.1f);
-			result |= ImGui.DragFloat3("Scale", ref scale, 0.01f);
 			IsEditing = result;
 			return result;
 		}

Ktisis/Util/GuiHelpers.cs
@@ -106,8 +106,10 @@ namespace Ktisis.Util
 			if (control->hkaAnimationControl.LocalTime >= durationLimit)
 				control->hkaAnimationControl.LocalTime = 0f;
 
+			ImGui.PushItemWidth(ImGui.GetContentRegionAvail().X - (ImGui.CalcTextSize("Speed").X + ImGui.GetFontSize() * .25f));
 			ImGui.SliderFloat("Seek", ref control->hkaAnimationControl.LocalTime, 0, durationLimit);
 			ImGui.SliderFloat("Speed", ref control->PlaybackSpeed, 0f, 0.999f);
+			ImGui.PopItemWidth();
 		}
 
 		// HoverPopupWindow Method

commit 29cc5a30f3f8a0eddf06bb43aa5e2f72688d9947

Fix frame extreme widening bug with high item spacing, introduced in 82858d6

Ktisis/Interface/Components/TransformTable.cs
@@ -44,8 +44,7 @@ namespace Ktisis.Interface.Components {
 			sizes[0] = GuiHelpers.CalcIconSize(iconPos).X;
 			sizes[1] = GuiHelpers.CalcIconSize(iconRot).X;
 			sizes[2] = GuiHelpers.CalcIconSize(iconSca).X;
-			var rightOffset = sizes.Max() + (ImGui.GetStyle().WindowPadding.X * .75f) + 0.1f;
-			
+			var rightOffset = GuiHelpers.GetRightOffset(sizes.Max());
 
 			ImGui.PushItemWidth(ImGui.GetContentRegionAvail().X - rightOffset);
 			result |= ImGui.DragFloat3("##Position", ref Position, 0.0005f);

Ktisis/Interface/Windows/Workspace.cs
@@ -67,7 +67,7 @@ namespace Ktisis.Interface.Windows {
 				if(Ktisis.IsInGPose) ImGui.PushStyleColor(ImGuiCol.Text, pose ? ColGreen : ColRed);
 				var label = pose ? "Posing" : "Not Posing";
 				float toggleWidth = ImGui.GetFrameHeight() * 1.55f;
-				float offsetWidth  = toggleWidth + (ImGui.GetStyle().WindowPadding.X * .75f) + 0.1f;
+				float offsetWidth = GuiHelpers.GetRightOffset(toggleWidth);
 				GuiHelpers.TextRight(label, offsetWidth);
 				if (Ktisis.IsInGPose) ImGui.PopStyleColor();
 				ImGui.SameLine();

Ktisis/Util/GuiHelpers.cs
@@ -110,6 +110,13 @@ namespace Ktisis.Util
 			return show;
 		}
 
+		// this function usually goes with TextRight or inputs/drag to calculate a safe right margin
+		public static float GetRightOffset(float calculatedTextSize) {
+			return calculatedTextSize
+				+ ImGui.GetStyle().ItemSpacing.X
+				//+ ImGui.GetStyle().WindowPadding.X
+				+ 0.5f; // extra safety
+		}
 		public static void TextRight(string text, float offset = 0) {
 			offset = ImGui.GetContentRegionAvail().X - offset - ImGui.CalcTextSize(text).X;
 			ImGui.SetCursorPosX(ImGui.GetCursorPosX() + offset);
@@ -151,7 +158,7 @@ namespace Ktisis.Util
 			if (control->hkaAnimationControl.LocalTime >= durationLimit)
 				control->hkaAnimationControl.LocalTime = 0f;
 
-			ImGui.PushItemWidth(ImGui.GetContentRegionAvail().X - (ImGui.CalcTextSize("Speed").X + ImGui.GetFontSize() * .25f));
+			ImGui.PushItemWidth(ImGui.GetContentRegionAvail().X - GetRightOffset(ImGui.CalcTextSize("Speed").X));
 			ImGui.SliderFloat("Seek", ref control->hkaAnimationControl.LocalTime, 0, durationLimit);
 			ImGui.SliderFloat("Speed", ref control->PlaybackSpeed, 0f, 0.999f);
 			ImGui.PopItemWidth();