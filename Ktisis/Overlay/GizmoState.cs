7ec6d59bceae53e8c83f5d74d5365d91a135fc00
a <spysapizfunny@gmail.com>
2022-11-12 22:51:55 +0100

    deleted gizmo graph. it was broken due to indentation change.

--- Ktisis/Overlay/GizmoState.cs
+++ Ktisis/Overlay/GizmoState.cs
@@ -1,26 +1,5 @@
 ﻿namespace Ktisis.Overlay
 {
-	/* Guizmo State machine:
-								^
-								|
-								|event: OnGuizmoChange(GuizmoState)
-								|
-								|
-								|
-					IsUsing()	|
-			 +----------------+---------+
-			 |                          |
-			 |                          v
-			Idle                    Editing
-			 ^                          |
-			 |                          |
-			 +------+-------------------+
-					|      !IsUsing()
-					|
-					|event: OnGuizmoChange(GuizmoState)
-					|
-					v
- */
 	public enum GizmoState
 	{
 		IDLE,
