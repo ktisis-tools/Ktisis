87919f3c28623b0d6628001aee100bf1bd368df6
edited <spysapizfunny@gmail.com>
2022-11-14 18:24:59 +0100

    Removed a weirdly used keyword. It most likely got introduced during a copy paste.

--- Ktisis/Events/EventManager.cs
+++ Ktisis/Events/EventManager.cs
@@ -2,7 +2,7 @@
 
 namespace Ktisis.Events {
 	public static class EventManager {
-		public unsafe delegate void GPoseChange(ActorGposeState state);
+		public delegate void GPoseChange(ActorGposeState state);
 		public static GPoseChange? OnGPoseChange = null;
 
 		public static void FireOnGposeChangeEvent(ActorGposeState state) {
