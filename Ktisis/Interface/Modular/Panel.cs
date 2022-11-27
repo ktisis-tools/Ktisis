c27e01f46e500d4b6ea4ffbda5ba33f6dc5ead6c
Emyka <emyka4@outlook.com>
2022-11-27 12:45:43 +0000

    New Modular panels for GizmoOperations, ControlButtonsExtra, HandleEmpty

--- Ktisis/Interface/Modular/Panel.cs
+++ Ktisis/Interface/Modular/Panel.cs
@@ -1,7 +1,12 @@
-﻿namespace Ktisis.Interface.Modular {
+﻿using ImGuiNET;
+
+namespace Ktisis.Interface.Modular {
 	internal class Panel {
 
 		public static void ActorsList(ContentsInfo contentsInfo) => Components.ActorsList.Draw();
+		public static void GizmoOperations(ContentsInfo contentsInfo) => Components.ControlButtons.DrawGizmoOperations();
+		public static void ControlButtonsExtra(ContentsInfo contentsInfo) => Components.ControlButtons.DrawExtra();
+		public static void HandleEmpty(ContentsInfo contentsInfo) => ImGui.Text("       ");
 
 	}
 }
