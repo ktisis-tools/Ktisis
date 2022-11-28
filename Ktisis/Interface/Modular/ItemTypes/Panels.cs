b424dce421f9a4a5c9c16f2ae8ef88c0ea4df9dd
Emyka <emyka4@outlook.com>
2022-11-28 22:38:48 +0000

    Add usings for cosmetic

--- Ktisis/Interface/Modular/ItemTypes/Panels.cs
+++ Ktisis/Interface/Modular/ItemTypes/Panels.cs
@@ -1,20 +1,23 @@
 using ImGuiNET;
 
+using Ktisis.Interface.Components;
+using Ktisis.Interface.Windows.Workspace;
+
 namespace Ktisis.Interface.Modular.ItemTypes.Panel {
 	public class ActorList : IModularItem {
-		public void Draw() => Components.ActorsList.Draw();
+		public void Draw() => ActorsList.Draw();
 	}
 	public class ControlButtonsExtra : IModularItem {
-		public void Draw() => Components.ControlButtons.DrawExtra();
+		public void Draw() => ControlButtons.DrawExtra();
 	}
 	public class HandleEmpty : IModularItem {
 		public void Draw() => ImGui.Text("       ");
 	}
 	public class GizmoOperations : IModularItem {
-		public void Draw() => Components.ControlButtons.DrawGizmoOperations();
+		public void Draw() => ControlButtons.DrawGizmoOperations();
 	}
 	public class GposeTextIndicator : IModularItem {
-		public void Draw() => Windows.Workspace.Workspace.DrawGposeIndicator();
+		public void Draw() => Workspace.DrawGposeIndicator();
 	}
 
 }
