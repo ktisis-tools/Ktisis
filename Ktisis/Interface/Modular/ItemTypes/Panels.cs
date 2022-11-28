077c6b3cedc4008de91570e73177c14d049cd34d
Emyka <emyka4@outlook.com>
2022-11-28 22:32:06 +0000

    Cosmetic change in Panels.cs

--- Ktisis/Interface/Modular/ItemTypes/Panels.cs
+++ Ktisis/Interface/Modular/ItemTypes/Panels.cs
@@ -2,24 +2,16 @@ using ImGuiNET;
 
 namespace Ktisis.Interface.Modular.ItemTypes.Panel {
 	public class ActorList : IModularItem {
-		public void Draw() {
-			Components.ActorsList.Draw();
-		}
+		public void Draw() => Components.ActorsList.Draw();
 	}
 	public class ControlButtonsExtra : IModularItem {
-		public void Draw() {
-			Components.ControlButtons.DrawExtra();
-		}
+		public void Draw() => Components.ControlButtons.DrawExtra();
 	}
 	public class HandleEmpty : IModularItem {
-		public void Draw() {
-			ImGui.Text("       ");
-		}
+		public void Draw() => ImGui.Text("       ");
 	}
 	public class GizmoOperations : IModularItem {
-		public void Draw() {
-			Components.ControlButtons.DrawGizmoOperations();
-		}
+		public void Draw() => Components.ControlButtons.DrawGizmoOperations();
 	}
 
 }
