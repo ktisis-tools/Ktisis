7f80e388522705d2a3d6f2f7ea470c4c5c54d97c
Emyka <emyka4@outlook.com>
2022-11-28 21:36:47 +0000

    Move Advanced in DrawAdvanced()

--- Ktisis/Interface/Windows/Workspace/Workspace.cs
+++ Ktisis/Interface/Windows/Workspace/Workspace.cs
@@ -210,23 +210,31 @@ namespace Ktisis.Interface.Windows.Workspace
 
 			// Advanced
 			if (ImGui.CollapsingHeader("Advanced (Debug)")) {
-				if (ImGui.Button("Reset Current Pose") && actor->Model != null)
-					actor->Model->SyncModelSpace();
-
-				if (ImGui.Button("Set to Reference Pose") && actor->Model != null)
-					actor->Model->SyncModelSpace(true);
-
-				if (ImGui.Button("Store Pose") && actor->Model != null)
-					_TempPose.Store(actor->Model->Skeleton);
-				ImGui.SameLine();
-				if (ImGui.Button("Apply Pose") && actor->Model != null)
-					_TempPose.Apply(actor->Model->Skeleton);
-
-				if (ImGui.Button("Force Redraw"))
-					actor->Redraw();
+				DrawAdvanced();
 			}
 
 			ImGui.EndTabItem();
+		}
+
+
+		private unsafe static void DrawAdvanced() {
+			var actor = Ktisis.Target;
+			if (actor->Model == null) return;
+
+			if (ImGui.Button("Reset Current Pose") && actor->Model != null)
+				actor->Model->SyncModelSpace();
+
+			if (ImGui.Button("Set to Reference Pose") && actor->Model != null)
+				actor->Model->SyncModelSpace(true);
+
+			if (ImGui.Button("Store Pose") && actor->Model != null)
+				_TempPose.Store(actor->Model->Skeleton);
+			ImGui.SameLine();
+			if (ImGui.Button("Apply Pose") && actor->Model != null)
+				_TempPose.Apply(actor->Model->Skeleton);
+
+			if (ImGui.Button("Force Redraw"))
+				actor->Redraw();
 		}
 
 		// Transform Table actor and bone names display, actor related extra
