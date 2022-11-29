4f423e02e755587dca317b2e3f9a9a9484760b5c
Emyka <emyka4@outlook.com>
2022-11-29 23:49:10 +0000

    Add new container: TopScreenBar

--- Ktisis/Interface/Modular/ItemTypes/Container.cs
+++ Ktisis/Interface/Modular/ItemTypes/Container.cs
@@ -4,7 +4,7 @@ using ImGuiNET;
 
 namespace Ktisis.Interface.Modular.ItemTypes.BaseContainer {
 	public class Window : IModularItem, IModularContainer {
-		private readonly int windowID;
+		protected readonly int windowID;
 
 		public ImGuiWindowFlags DrawFlags { get; }
 		public List<IModularItem> Items { get; }
@@ -48,6 +48,30 @@ namespace Ktisis.Interface.Modular.ItemTypes.Container {
 	public class WindowBar : BaseContainer.Window {
 		public WindowBar(int windowID, string title, List<IModularItem> items) : base(windowID, ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.AlwaysAutoResize, title, items) { }
 		public WindowBar(int windowID, string title) : this(windowID, title, new()) { }
+		protected override void DrawItem(IModularItem item) {
+			ImGui.SameLine();
+			ImGui.SetCursorPosX(ImGui.GetCursorPosX() + (ImGui.GetStyle().ItemSpacing.X * 2));
+			base.DrawItem(item);
+		}
+	}
+	public class TopScreenBar : BaseContainer.Window {
+		public TopScreenBar(int windowID, string title, List<IModularItem> items) : base(windowID, ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoBringToFrontOnFocus | ImGuiWindowFlags.AlwaysAutoResize, title, items) { }
+		public TopScreenBar(int windowID, string title) : this(windowID, title, new()) { }
+		public override void Draw() {
+
+			ImGui.SetNextWindowPos(new(0));
+			ImGui.SetNextWindowSize(new(ImGui.GetIO().DisplaySize.X, -1));
+			ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 0);
+			ImGui.PushStyleVar(ImGuiStyleVar.WindowBorderSize, 0);
+			if (ImGui.Begin($"{this.Title}##ModularWindow##{this.windowID}", this.DrawFlags)) {
+				if (this.Items != null)
+					foreach (var item in this.Items) {
+						this.DrawItem(item);
+					}
+			}
+			ImGui.End();
+			ImGui.PopStyleVar(2);
+		}
 		protected override void DrawItem(IModularItem item) {
 			ImGui.SameLine();
 			ImGui.SetCursorPosX(ImGui.GetCursorPosX() + (ImGui.GetStyle().ItemSpacing.X * 2));
