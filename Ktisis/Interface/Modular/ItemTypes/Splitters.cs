f78592f5487b1f671e929d4626cd2d7f859be534
Emyka <emyka4@outlook.com>
2022-11-29 21:31:49 +0000

    Fix Splitters not overriding Draw()

--- Ktisis/Interface/Modular/ItemTypes/Splitters.cs
+++ Ktisis/Interface/Modular/ItemTypes/Splitters.cs
@@ -11,7 +11,7 @@ namespace Ktisis.Interface.Modular.ItemTypes {
 
 		public BaseSplitter(List<IModularItem> items) => this.Items = items;
 
-		public void Draw() {
+		virtual public void Draw() {
 			if (this.Items != null)
 				foreach (var item in this.Items) {
 					this.DrawItem(item);
@@ -28,7 +28,7 @@ namespace Ktisis.Interface.Modular.ItemTypes.Splitter {
 	public class Columns : BaseSplitter {
 		public Columns(List<IModularItem> items) : base(items) { }
 
-		public new void Draw() {
+		public override void Draw() {
 			if (this.Items != null) {
 				ImGui.Columns(this.Items.Count);
 				foreach (var item in this.Items) {
