a8289457222da8fe58f267b28120d97f3f91f3d8
chirp <ch1rp@protonmail.com>
2022-12-01 11:26:22 +0000

    create AnamCharaFile, enum conversions dict

---
+++ Ktisis/Data/Files/AnamCharaFile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Ktisis.Data.Files {
+	public class AnamCharaFile {
+		private static Dictionary<string, string> EnumConversions = new() {
+			{ "LegacyTattoo", "Legacy" },
+			{ "Lalafel", "Lalafell" },
+			{ "SeekerOfTheSun", "SunSeeker" },
+			{ "KeeperOfTheMoon", "MoonKeeper" },
+			{ "Helions", "Helion" },
+			{ "TheLost", "Lost" }
+		};
+	}
+}
