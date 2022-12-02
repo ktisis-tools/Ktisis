2e2c54ffffd201b5f58dd0df592df99d34fb6ca3
chirp <ch1rp@protonmail.com>
2022-12-02 13:36:13 +0000

    include 0x00 in NOP check

--- Ktisis/Interop/StaticOffsets.cs
+++ Ktisis/Interop/StaticOffsets.cs
@@ -8,13 +8,13 @@ namespace Ktisis.Interop {
 		internal static IntPtr CharaDatData;
 
 		// If this is NOP'd, Anam posing is enabled.
-		internal static IntPtr FreezePosition;
-		internal static IntPtr FreezeRotation;
-		internal static IntPtr FreezeScale;
+		internal unsafe static byte* FreezePosition;
+		internal unsafe static byte* FreezeRotation;
+		internal unsafe static byte* FreezeScale;
 
-		internal unsafe static bool IsPositionFrozen => FreezePosition != IntPtr.Zero && *(byte*)FreezePosition == 0x90;
-		internal unsafe static bool IsRotationFrozen => FreezeRotation != IntPtr.Zero && *(byte*)FreezeRotation == 0x90;
-		internal unsafe static bool IsScalingFrozen => FreezeScale != IntPtr.Zero && *(byte*)FreezeScale == 0x90;
+		internal unsafe static bool IsPositionFrozen => FreezePosition != null && *FreezePosition == 0x90 || *FreezePosition == 0x00;
+		internal unsafe static bool IsRotationFrozen => FreezeRotation != null && *FreezeRotation == 0x90 || *FreezeRotation == 0x00;
+		internal unsafe static bool IsScalingFrozen => FreezeScale != null && *FreezeScale == 0x90 || *FreezeScale == 0x00;
 
 		internal unsafe static bool IsAnamPosing => IsPositionFrozen || IsRotationFrozen || IsScalingFrozen;
 
@@ -23,9 +23,9 @@ namespace Ktisis.Interop {
 			var qword_14200E548 = *(IntPtr*)Services.SigScanner.GetStaticAddressFromSig("48 8B 05 ?? ?? ?? ?? 48 C7 44 24 24 05 00 00 00 C6 84 24");
 			CharaDatData = *(IntPtr*)(qword_14200E548 + 1392);
 
-			FreezePosition = Services.SigScanner.ScanText("41 0F 29 24 12");
-			FreezeRotation = Services.SigScanner.ScanText("41 0F 29 5C 12 10");
-			FreezeScale = Services.SigScanner.ScanText("41 0F 29 44 12 20");
+			FreezePosition = (byte*)Services.SigScanner.ScanText("41 0F 29 24 12");
+			FreezeRotation = (byte*)Services.SigScanner.ScanText("41 0F 29 5C 12 10");
+			FreezeScale = (byte*)Services.SigScanner.ScanText("41 0F 29 44 12 20");
 		}
 	}
 }
