71caa1c6cff6d9ee0ace618ca46ffc3259a6426f
Fia3D <fia3d@pm.me>
2022-12-15 20:48:53 +0100

    Fold main buttons into ToolbarWindow

--- Ktisis/Interface/Windows/Toolbar/ToolbarWindow.cs
+++ Ktisis/Interface/Windows/Toolbar/ToolbarWindow.cs
@@ -1,9 +1,13 @@
 using System.Numerics;
 
+using Dalamud.Interface;
+
 using ImGuiNET;
 
+using ImGuizmoNET;
+
+using Ktisis.History;
 using Ktisis.Interface.Components;
-using Ktisis.Interface.Components.Toolbar;
 using Ktisis.Interface.Windows.ActorEdit;
 using Ktisis.Interop.Hooks;
 using Ktisis.Overlay;
@@ -101,7 +105,7 @@ namespace Ktisis.Interface.Windows.Toolbar {
 			// ----------------------------------------------------------------------------
 			// ----------------------------------------------------------------------------
 
-			ToolbarControlButtons.Draw();
+			DrawMainButtons();
 
 			ImGui.SameLine(0, ImGui.GetFontSize());
 
@@ -125,6 +129,48 @@ namespace Ktisis.Interface.Windows.Toolbar {
 			if (GuiHelpers.IconButtonTooltip(IconsPool.Settings, "Ktisis main window", ControlButtons.ButtonSize))
 				Workspace.Workspace.Toggle();
 			ImGui.End();
+		}
+
+		public static void DrawMainButtons() {
+			// Operations
+			ControlButtons.ButtonChangeOperation(OPERATION.TRANSLATE, IconsPool.Position);
+			ImGui.SameLine();
+			ControlButtons.ButtonChangeOperation(OPERATION.ROTATE, IconsPool.Rotation);
+			ImGui.SameLine();
+			ControlButtons.ButtonChangeOperation(OPERATION.SCALE, IconsPool.Scale);
+			ImGui.SameLine();
+			ControlButtons.VerticalAlignTextOnButtonSize(0.9f);
+			if (GuiHelpers.IconButtonTooltip(IconsPool.DownMore, "Show transform table ")) {
+				TransformWindow.Toggle();
+			}
+			ImGui.SameLine(0, ImGui.GetFontSize());
+			ControlButtons.VerticalAlignTextOnButtonSize(0.9f);
+			if (GuiHelpers.IconButtonTooltip(IconsPool.Undo, "Undo previous action")) {
+				HistoryManager.Undo();
+			}
+			ImGui.SameLine();
+			ControlButtons.VerticalAlignTextOnButtonSize(0.9f);
+			if (GuiHelpers.IconButtonTooltip(IconsPool.Redo, "Redo previous action")) {
+				HistoryManager.Redo();
+			}
+			ImGui.SameLine(0, ImGui.GetFontSize());
+			// Extra Options
+			var gizmoMode = Ktisis.Configuration.GizmoMode;
+			if (GuiHelpers.IconButtonTooltip(gizmoMode == MODE.WORLD ? FontAwesomeIcon.Globe : FontAwesomeIcon.Home, "Local / World orientation mode switch.", ControlButtons.ButtonSize))
+				Ktisis.Configuration.GizmoMode = gizmoMode == MODE.WORLD ? MODE.LOCAL : MODE.WORLD;
+
+			ImGui.SameLine();
+
+			var showSkeleton = Ktisis.Configuration.ShowSkeleton;
+			if (showSkeleton) ImGui.PushStyleColor(ImGuiCol.Text, GuiHelpers.VisibleCheckmarkColor());
+			if (GuiHelpers.IconButton(showSkeleton ? FontAwesomeIcon.Eye : FontAwesomeIcon.EyeSlash, ControlButtons.ButtonSize))
+				Skeleton.Toggle();
+			if (showSkeleton) ImGui.PopStyleColor();
+			GuiHelpers.Tooltip((showSkeleton ? "Hide" : "Show") + " skeleton lines and bones.");
+
+			ImGui.SameLine();
+
+			ControlButtons.DrawSiblingLink();
 		}
 	}
 }
