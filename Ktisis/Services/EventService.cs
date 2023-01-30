31cf60f3e388823bc387737ecefb5d846be9f526
chirp <ch1rp@protonmail.com>
2023-01-30 23:17:18 +0000

    listen for gpose change on framework update

--- Ktisis/Services/EventService.cs
+++ Ktisis/Services/EventService.cs
@@ -5,13 +5,13 @@ using Ktisis.Structs.Input;
 namespace Ktisis.Services {
 	public static class EventService {
 		public delegate void GPoseChange(bool isInGPose);
-		public static GPoseChange? OnGPoseChange = null;
+		public static GPoseChange? OnGPoseChange;
 
 		public delegate void TransformationMatrixChange(bool state);
-		public static TransformationMatrixChange? OnTransformationMatrixChange = null;
+		public static TransformationMatrixChange? OnTransformationMatrixChange;
 
 		public delegate void GizmoChange(bool isEditing);
-		public static GizmoChange? OnGizmoChange = null;
+		public static GizmoChange? OnGizmoChange;
 
 		internal delegate bool KeyPressEvent(QueueItem e);
 		internal static KeyPressEvent? OnKeyPressed;
@@ -20,7 +20,9 @@ namespace Ktisis.Services {
 		internal static KeyReleaseEvent? OnKeyReleased;
 
 		internal delegate void FrameworkUpdate();
-		internal static FrameworkUpdate OnFrameworkUpdate = null!;
+		internal static FrameworkUpdate? OnFrameworkUpdate;
+
+		// Init & Dispose
 
 		internal static void Init()
 			=> DalamudServices.Framework.Update += InvokeFrameworkUpdate;
@@ -28,8 +30,20 @@ namespace Ktisis.Services {
 		internal static void Dispose()
 			=> DalamudServices.Framework.Update -= InvokeFrameworkUpdate;
 
+		// Listen to framework updates
+
+		private static bool _GPoseState = false;
+
 		private static void InvokeFrameworkUpdate(object _) {
-			OnFrameworkUpdate();
+			var inGpose = GPoseService.IsInGPose;
+			if (inGpose != _GPoseState) {
+				_GPoseState = inGpose;
+				if (OnGPoseChange != null)
+					OnGPoseChange(inGpose);
+			}
+
+			if (OnFrameworkUpdate != null)
+				OnFrameworkUpdate();
 		}
 	}
 }
