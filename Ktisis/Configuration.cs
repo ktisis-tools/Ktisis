2c6a8513b0d444b7d551cc9a007c58f4f2ea69a2
chirp <ch1rp@protonmail.com>
2023-01-30 23:20:33 +0000

    remove unnecessary getters/setters in config

--- Ktisis/Configuration.cs
+++ Ktisis/Configuration.cs
@@ -22,51 +22,51 @@ namespace Ktisis {
 		public const int CurVersion = 2;
 		public int Version { get; set; } = CurVersion;
 
-		public bool IsFirstTimeInstall { get; set; } = true;
-		public string LastPluginVer { get; set; } = "";
+		public bool IsFirstTimeInstall = true;
+		public string LastPluginVer = "";
 
 		// Interface
 		[Obsolete("Replaced by AutoOpenCtor")]
-		public bool AutoOpen { get; set; } = true;
+		public bool AutoOpen = true;
 		[Obsolete("Replaced by OpenKtisisMethod")]
-		public bool AutoOpenCtor { get; set; } = false;
-		public OpenKtisisMethod OpenKtisisMethod { get; set; } = OpenKtisisMethod.OnEnterGpose;
+		public bool AutoOpenCtor = false;
+		public OpenKtisisMethod OpenKtisisMethod = OpenKtisisMethod.OnEnterGpose;
 
-		public bool DisplayCharName { get; set; } = true;
-		public bool CensorNsfw { get; set; } = false;
+		public bool DisplayCharName = true;
+		public bool CensorNsfw = false;
 
-		public bool TransformTableDisplayMultiplierInputs { get; set; } = false;
-		public float TransformTableBaseSpeedPos { get; set; } = 0.0005f;
-		public float TransformTableBaseSpeedRot { get; set; } = 0.1f;
-		public float TransformTableBaseSpeedSca { get; set; } = 0.001f;
-		public float TransformTableModifierMultCtrl { get; set; } = 0.1f;
-		public float TransformTableModifierMultShift { get; set; } = 10f;
-		public int TransformTableDigitPrecision { get; set; } = 3;
-		public float CustomWidthMarginDebug { get; set; } = 0.1f;
+		public bool TransformTableDisplayMultiplierInputs = false;
+		public float TransformTableBaseSpeedPos = 0.0005f;
+		public float TransformTableBaseSpeedRot = 0.1f;
+		public float TransformTableBaseSpeedSca = 0.001f;
+		public float TransformTableModifierMultCtrl = 0.1f;
+		public float TransformTableModifierMultShift = 10f;
+		public int TransformTableDigitPrecision = 3;
+		public float CustomWidthMarginDebug = 0.1f;
 
 		// Input
-		public bool EnableKeybinds { get; set; } = false;
-		public Dictionary<Input.Purpose, List<VirtualKey>> KeyBinds { get; set; } = new();
+		public bool EnableKeybinds = false;
+		public Dictionary<Input.Purpose, List<VirtualKey>> KeyBinds = new();
 
-		public bool DisableChangeTargetOnLeftClick { get; set; } = false;
-		public bool DisableChangeTargetOnRightClick { get; set; } = false;
+		public bool DisableChangeTargetOnLeftClick = false;
+		public bool DisableChangeTargetOnRightClick = false;
 
 		// Overlay
 
-		public bool DrawLinesOnSkeleton { get; set; } = true;
-		public bool DrawLinesWithGizmo { get; set; } = true;
-		public bool DrawDotsWithGizmo { get; set; } = true;
+		public bool DrawLinesOnSkeleton = true;
+		public bool DrawLinesWithGizmo = true;
+		public bool DrawDotsWithGizmo = true;
 
-		public float SkeletonLineThickness { get; set; } = 2.0F;
-		public float SkeletonLineOpacity { get; set; } = 0.95F;
-		public float SkeletonLineOpacityWhileUsing { get; set; } = 0.15F;
-		public float SkeletonDotRadius { get; set; } = 3.0F;
+		public float SkeletonLineThickness = 2.0F;
+		public float SkeletonLineOpacity = 0.95F;
+		public float SkeletonLineOpacityWhileUsing = 0.15F;
+		public float SkeletonDotRadius = 3.0F;
 
 		// References
 		// The reference Key creates a uniqueness constraint for imgui window IDs for each reference.
-		public Dictionary<int, ReferenceInfo> References { get; set; } = new();
-		public float ReferenceAlpha { get; set; } = 1.0f;
-		public bool ReferenceHideDecoration { get; set; } = false;
+		public Dictionary<int, ReferenceInfo> References = new();
+		public float ReferenceAlpha = 1.0f;
+		public bool ReferenceHideDecoration = false;
 		public int NextReferenceKey => References.Count == 0 ? 0 : References.Max(x => x.Key) + 1;
 
 		public Vector4 GetCategoryColor(Bone bone) {
@@ -105,43 +105,43 @@ namespace Ktisis {
 
 		// Gizmo
 
-		public MODE GizmoMode { get; set; } = MODE.LOCAL;
-		public OPERATION GizmoOp { get; set; } = OPERATION.ROTATE;
+		public MODE GizmoMode = MODE.LOCAL;
+		public OPERATION GizmoOp = OPERATION.ROTATE;
 
-		public SiblingLink SiblingLink { get; set; } = SiblingLink.None;
+		public SiblingLink SiblingLink = SiblingLink.None;
 
-		public bool AllowAxisFlip { get; set; } = true;
+		public bool AllowAxisFlip = true;
 
 		// Language
 
-		public UserLocale Localization { get; set; } = UserLocale.English;
-		public ClientLanguage SheetLocale { get; set; } = ClientLanguage.English;
+		public UserLocale Localization = UserLocale.English;
+		public ClientLanguage SheetLocale = ClientLanguage.English;
 
 		public bool TranslateBones = true;
 
 		// UI memory
 
-		public bool ShowSkeleton { get; set; } = false;
+		public bool ShowSkeleton = false;
 		public Dictionary<string, bool> ShowBoneByCategory = new();
-		public bool LinkBoneCategoryColors { get; set; } = false;
-		public Vector4 LinkedBoneCategoryColor { get; set; } = new(1.0F, 1.0F, 1.0F, 0.5647059F);
+		public bool LinkBoneCategoryColors = false;
+		public Vector4 LinkedBoneCategoryColor = new(1.0F, 1.0F, 1.0F, 0.5647059F);
 		public Dictionary<string, Vector4> BoneCategoryColors = new();
 
-		public SaveModes CharaMode { get; set; } = SaveModes.All;
-		public PoseMode PoseMode { get; set; } = PoseMode.All;
-		public PoseTransforms PoseTransforms { get; set; } = PoseTransforms.Rotation;
+		public SaveModes CharaMode = SaveModes.All;
+		public PoseMode PoseMode = PoseMode.All;
+		public PoseTransforms PoseTransforms = PoseTransforms.Rotation;
 
-		public bool EnableParenting { get; set; } = true;
+		public bool EnableParenting = true;
 
-		public bool LinkedGaze { get; set; } = true;
+		public bool LinkedGaze = true;
 		
-		public bool ShowToolbar { get; set; } = false;
+		public bool ShowToolbar = false;
 
-		public Dictionary<string, string> SavedDirPaths { get; set; } = new();
+		public Dictionary<string, string> SavedDirPaths = new();
 
 		// Data memory
-		public Dictionary<string, GlamourDresser.GlamourPlate[]?>? GlamourPlateData { get; set; } = null;
-		public Dictionary<string, Dictionary<string, Vector3>> CustomBoneOffset { get; set; } = new();
+		public Dictionary<string, GlamourDresser.GlamourPlate[]?>? GlamourPlateData = null;
+		public Dictionary<string, Dictionary<string, Vector3>> CustomBoneOffset = new();
 
 		// Validate for changes in config versions.
 
