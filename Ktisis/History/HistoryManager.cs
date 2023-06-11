c36656d55fdd77f25723c5355822b8ccd95a5c57
chirp <ch1rp@protonmail.com>
2023-06-11 16:09:04 +0100

    Refactor HistoryManager

--- Ktisis/History/HistoryManager.cs
+++ Ktisis/History/HistoryManager.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 
 using Dalamud.Game.ClientState.Keys;
+using Dalamud.Logging;
 
 using Ktisis.Events;
 using Ktisis.Overlay;
@@ -14,12 +15,11 @@ namespace Ktisis.History {
 	public static class HistoryManager {
 		public static List<HistoryItem>? History { get; set; }
 		private static int _currentIdx = -1;
-		private static int _maxIdx = -1;
 		private static bool _currentState;
 		private static int _alternativeTimelinesCreated = 0;
 
-		public static bool CanRedo => _currentIdx < _maxIdx;
-		public static bool CanUndo => _currentIdx >= 1;
+		public static bool CanRedo => History != null && _currentIdx < History.Count - 1;
+		public static bool CanUndo => _currentIdx > -1;
 
 		// Init & Dispose
 
@@ -56,30 +56,32 @@ namespace Ktisis.History {
 		public static void Redo() {
 			if (!CanRedo)
 				return;
-
+			
 			_currentIdx++;
 			UpdateSkeleton(false);
+
+			PluginLog.Information($"Redo: Now on {_currentIdx}");
 		}
 
 		public static void Undo() {
 			if (!CanUndo)
 				return;
-
+			
 			UpdateSkeleton(true);
 			_currentIdx--;
+			
+			PluginLog.Information($"Undo: Now on {_currentIdx}");
 		}
 
 		internal static void OnGPoseChange(bool isInGpose) {
 			Logger.Verbose("Clearing previous history...");
-			_currentIdx = 0;
-			_maxIdx = 0;
+			_currentIdx = -1;
 			History = new List<HistoryItem>();
 		}
 
 		//TODO: Find a way to know what's the currently modified item to be able to add the correct entry to the history.
-
-		public static ActorBone? CurrentBone = null;
-
+		
+		private static ActorBone? ActiveEdit = null;
 		private static void OnGizmoChange(bool isEditing) {
 			if (!PoseHooks.PosingEnabled && !PoseHooks.AnamPosingEnabled) return;
 			if (History == null) return;
@@ -91,28 +93,16 @@ namespace Ktisis.History {
 			}
 
 			if (isEditing && !_currentState) {
-				if (_maxIdx != _currentIdx) createNewTimeline();
-				UpdateHistory(HistoryItemType.ActorBone);
+				ActiveEdit = (ActorBone?)HistoryItemFactory.Create(HistoryItemType.ActorBone);
 			}
 
-			if (!isEditing && _currentState) {
-				var x = _currentIdx - 1;
-				if (x >= 0 && x < History.Count)
-					((ActorBone)History[x]).SetMatrix(false);
+			if (!isEditing && _currentState && ActiveEdit != null) {
+				ActiveEdit.SetMatrix(false);
+				AddEntryToHistory(ActiveEdit);
+				ActiveEdit = null;
 			}
 
 			_currentState = isEditing;
-		}
-
-		private static void UpdateHistory(HistoryItemType entryType) {
-			try {
-				var entryToAdd = HistoryItemFactory.Create(HistoryItemType.ActorBone);
-				if (entryToAdd != null)
-					AddEntryToHistory(entryToAdd);
-			} catch (ArgumentException e) {
-				Logger.Fatal(e.Message);
-				return;
-			}
 		}
 
 		// Methods
@@ -122,26 +112,22 @@ namespace Ktisis.History {
 				Logger.Warning("Attempted to add an entry to an uninitialised history list.");
 				return;
 			}
-			History.Insert(_maxIdx, historyItem);
+			
 			_currentIdx++;
-			_maxIdx++;
+			if (_currentIdx < History.Count)
+				CreateNewTimeline();
+			History.Add(historyItem);
+			
 		}
 
 		private static void UpdateSkeleton(bool undo) {
-			History![_currentIdx - 1].Update(undo);
+			PluginLog.Information($"{undo} {_currentIdx}");
+			History![_currentIdx].Update(undo);
 		}
 
-		private static void createNewTimeline() {
+		private static void CreateNewTimeline() {
 			if (History is null) return;
-
-			Logger.Verbose($"By changing the past, you've created a different future. You've created {_alternativeTimelinesCreated} different timelines.");
-
-			var newHistory = History.Select(e => e.Clone()).ToList().GetRange(0, _currentIdx + 1);
-			HistoryItem currentElem = newHistory[_currentIdx];
-			var newMaxIdx = _currentIdx;
-			History = newHistory.GetRange(0, newMaxIdx);
-			_maxIdx = newMaxIdx;
-			_currentIdx = newMaxIdx;
+			History = History.GetRange(0, _currentIdx).Select(e => e.Clone()).ToList();
 		}
 	}
 }
