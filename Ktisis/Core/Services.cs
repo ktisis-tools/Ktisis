623461a8d213e9d8429579989cd5f857fde8b172
chirp <ch1rp@protonmail.com>
2023-06-22 20:27:54 +0100

    Implement singleton and service managers

---
+++ Ktisis/Core/Services.cs
@@ -0,0 +1,19 @@
+﻿using Dalamud.IoC;
+using Dalamud.Game;
+using Dalamud.Plugin;
+using Dalamud.Game.Gui;
+using Dalamud.Game.Command;
+
+using Ktisis.Interop;
+using Ktisis.Core.Singletons;
+
+namespace Ktisis.Core;
+
+public class Services : ServiceProvider {
+	[PluginService] internal static GameGui GameGui { get; private set; } = null!;
+	[PluginService] internal static SigScanner SigScanner { get; private set; } = null!;
+	[PluginService] internal static CommandManager CommandManager { get; private set; } = null!;
+	[PluginService] internal static DalamudPluginInterface PluginInterface { get; private set; } = null!;
+
+	[Service(ServiceFlags.Critical)] internal static InteropService Interop = null!;
+}
---
+++ Ktisis/Core/Singletons/Singleton.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Ktisis.Core.Singletons; 
+
+public abstract class Singleton : IDisposable {
+	public virtual void Init() { }
+	public virtual void Dispose() { }
+}
+
+public abstract class Service : Singleton { }
