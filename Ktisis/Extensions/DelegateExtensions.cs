435cf2d501188139f37e329302345756e98a296a
chirp <ch1rp@protonmail.com>
2023-06-24 18:26:36 +0100

    Implement Delegate.InvokeSafely extension

---
+++ Ktisis/Extensions/DelegateExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+
+using Dalamud.Logging;
+
+namespace Ktisis.Extensions;
+
+public static class DelegateExtensions {
+	public static void InvokeSafely(this Delegate @delegate, params object?[]? args) {
+		foreach (var invoke in @delegate.GetInvocationList()) {
+			try {
+				invoke.DynamicInvoke(args);
+			} catch (Exception e) {
+				PluginLog.Error($"Error in invocation of {@delegate.GetType().Name}:\n{e}");
+			}
+		}
+	}
+}
