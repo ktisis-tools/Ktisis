35c4a55d257fe23f98458993dfca08a176c567a8
chirp <ch1rp@protonmail.com>
2023-06-24 18:26:36 +0100

    Implement Delegate.InvokeSafely extension

--- Ktisis/Events/Providers/FrameworkEventProvider.cs
+++ Ktisis/Events/Providers/FrameworkEventProvider.cs
@@ -1,15 +1,18 @@
+using JetBrains.Annotations;
+
 using Dalamud.Game;
 
 using Ktisis.Core;
 using Ktisis.Events.Attributes;
+using Ktisis.Extensions;
 
 namespace Ktisis.Events.Providers; 
 
 public delegate void FrameworkEvent(Framework framework);
 
 public class FrameworkEventProvider : EventProvider {
-	[EventEmitter]
-	private event FrameworkEvent? Event;
+	[EventEmitter, UsedImplicitly]
+	private event FrameworkEvent? FrameworkEvent;
 	
 	public override void Setup() {
 		Services.Framework.Update += OnFrameworkUpdate;
@@ -17,7 +20,7 @@ public class FrameworkEventProvider : EventProvider {
 
 	private void OnFrameworkUpdate(Framework framework) {
 		if (!Services.Ready || IsDisposed) return;
-		Event?.Invoke(framework);
+		FrameworkEvent?.InvokeSafely(framework);
 	}
 
 	protected override void OnDispose() {
