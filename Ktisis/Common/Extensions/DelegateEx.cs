24693fe1fdfccb8ef47e6154e09eea875abd140f
chirp <ch1rp@protonmail.com>
2023-06-27 11:16:36 +0100

    Shorten names for extensions

--- Ktisis/Common/Extensions/DelegateEx.cs
+++ Ktisis/Common/Extensions/DelegateEx.cs
@@ -4,7 +4,7 @@ using Dalamud.Logging;
 
 namespace Ktisis.Common.Extensions;
 
-public static class DelegateExtensions {
+public static class DelegateEx {
 	public static void InvokeSafely(this Delegate @delegate, params object?[]? args) {
 		foreach (var invoke in @delegate.GetInvocationList()) {
 			try {
