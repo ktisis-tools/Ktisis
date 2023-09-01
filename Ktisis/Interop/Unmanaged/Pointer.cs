207844c62f0ec9595c7a06f8eabc62119d3b3efe
chirp <72366111+chirpxiv@users.noreply.github.com>
2023-09-01 18:48:37 +0100

    Implement null guard for `Pointer<T>.Data`

--- Ktisis/Interop/Unmanaged/Pointer.cs
+++ Ktisis/Interop/Unmanaged/Pointer.cs
@@ -1,12 +1,19 @@
+using System;
+
 namespace Ktisis.Interop.Unmanaged;
 
 public class Pointer<T> where T : unmanaged {
 	public nint Address;
 
-	public unsafe bool IsNull => this.Data == null;
+	public unsafe bool Equals(T* ptr) => (T*)this.Address == ptr;
+	public unsafe bool IsNull => this.Address == 0;
 
 	public unsafe T* Data {
-		get => (T*)this.Address;
+		get {
+			if (this.IsNull)
+				throw new Exception("Attempted to access data for null pointer.");
+			return (T*)this.Address;
+		}
 		set => this.Address = (nint)value;
 	}
 
