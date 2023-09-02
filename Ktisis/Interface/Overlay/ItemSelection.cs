de77157eac935f6db7828487513868b20c3fa386
chirp <72366111+chirpxiv@users.noreply.github.com>
2023-09-02 19:44:47 +0100

    Rework `DotSelection` -> `ItemSelection`

---
+++ Ktisis/Interface/Overlay/ItemSelection.cs
@@ -0,0 +1,204 @@
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+
+using Dalamud.Interface;
+using Dalamud.Logging;
+
+using ImGuiNET;
+
+using Ktisis.Data;
+using Ktisis.Data.Config.Display;
+using Ktisis.Common.Extensions;
+using Ktisis.Interface.Widgets;
+using Ktisis.Scene.Objects;
+using Ktisis.Scene.Impl;
+using Ktisis.Services;
+
+namespace Ktisis.Interface.Overlay;
+
+public delegate void OnItemSelectedHandler(SceneObject item);
+
+public class ItemSelection {
+	// Constructor
+
+	private readonly CameraService _camera;
+	private readonly DataService _data;
+
+	public ItemSelection(CameraService _camera, DataService _data) {
+		this._camera = _camera;
+		this._data = _data;
+	}
+
+	// Events
+
+	public event OnItemSelectedHandler? OnItemSelected;
+
+	// Items
+
+	private readonly List<ItemSelect> Items = new();
+
+	public void Clear() => this.Items.Clear();
+
+	public unsafe void AddItem(SceneObject item, Vector3 worldPos) {
+		var camera = this._camera.GetSceneCamera();
+		if (camera == null) return;
+
+		if (!camera->WorldToScreen(worldPos, out var pos2d))
+			return;
+
+		var dist = Vector3.Distance(camera->Object.Position, worldPos);
+		var select = new ItemSelect(item, new Vector3(pos2d, dist));
+		this.Items.Add(select);
+	}
+
+	// Draw
+
+	private int ScrollIndex;
+
+	public void Draw() {
+		var config = this._data.GetConfig();
+		var drawList = ImGui.GetWindowDrawList();
+
+		var isHovering = false;
+		foreach (var item in this.Items) {
+			var display = config.GetItemDisplay(item.Item.ItemType);
+			var pos2d = new Vector2(item.ScreenPos.X, item.ScreenPos.Y);
+
+			var isSelect = item.Item.IsSelected();
+			item.IsHovered = display.Mode switch {
+				DisplayMode.Dot => DrawPrimDot(drawList, pos2d, display),
+				DisplayMode.Icon => DrawIconDot(drawList, pos2d, display, isSelect),
+				_ => false
+			};
+
+			isHovering |= item.IsHovered;
+		}
+
+		if (isHovering)
+			DrawSelectWindow(this.Items.Where(item => item.IsHovered).ToList());
+
+		this.Clear();
+	}
+
+	private void DrawSelectWindow(List<ItemSelect> list) {
+		if (ImGuizmo.Gizmo.IsUsing || ImGuizmo.Gizmo.IsOver || list.Count == 0)
+			return;
+
+		var begin = false;
+		try {
+			ImGui.SetNextWindowPos(ImGui.GetMousePos().AddX(20));
+			ImGui.SetNextWindowSize(-Vector2.One, ImGuiCond.Always);
+			begin = ImGui.Begin("Hover", ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoFocusOnAppearing);
+			if (begin) DrawSelectList(list);
+		} catch (Exception err) {
+			PluginLog.Error($"Error while drawing select list:\n{err}");
+		} finally {
+			if (begin) ImGui.End();
+		}
+	}
+
+	private void DrawSelectList(List<ItemSelect> list) {
+		// Sort objects by those closest to the camera.
+		//  TODO: Configuration.OrderBoneListByDistance
+		list.Sort((a, b) => {
+			if (Math.Abs(a.ScreenPos.Z - b.ScreenPos.Z) > 0.01f)
+				return a.ScreenPos.Z < b.ScreenPos.Z ? -1 : 1;
+			return a.SortPriority - b.SortPriority;
+		});
+
+		// Handle mouse wheel input and clamp scroll index
+		this.ScrollIndex -= (int)ImGui.GetIO().MouseWheel;
+		if (this.ScrollIndex >= list.Count)
+			this.ScrollIndex = 0;
+		else if (this.ScrollIndex < 0)
+			this.ScrollIndex = list.Count - 1;
+
+		// Capture mouse input to intercept mouse clicks.
+		ImGui.SetNextFrameWantCaptureMouse(true);
+
+		// Check for mouse click.
+		var isClick = ImGui.IsMouseReleased(ImGuiMouseButton.Left);
+
+		for (var i = 0; i < list.Count; i++) {
+			var item = list[i];
+			var isSelect = i == this.ScrollIndex;
+			ImGui.Selectable(item.Name, isSelect);
+			if (isSelect && isClick)
+				this.OnItemSelected?.Invoke(item.Item);
+		}
+	}
+
+	// Draw UI dot
+
+	private const int HoverPadding = 6;
+
+	private bool IsHovering(Vector2 pos2d, float radius) => ImGui.IsMouseHoveringRect(
+		pos2d.Add(-radius - HoverPadding),
+		pos2d.Add(radius + HoverPadding)
+	);
+
+	private bool DrawPrimDot(ImDrawListPtr drawList, Vector2 pos2d, ItemDisplay display) {
+		// TODO
+		const float radius = 7f;
+
+		drawList.AddCircleFilled(
+			pos2d,
+			radius,
+			display.Color,
+			64
+		);
+
+		drawList.AddCircle(
+			pos2d,
+			radius,
+			0xFF000000,
+			64,
+			1f
+		);
+
+		return IsHovering(pos2d, radius);
+	}
+
+	private bool DrawIconDot(ImDrawListPtr drawList, Vector2 pos2d, ItemDisplay display, bool isSelect = false) {
+		var size = Icons.CalcIconSize(display.Icon);
+		var radius = UiBuilder.IconFont.FontSize;
+
+		var isHover = IsHovering(pos2d, radius);
+
+		drawList.AddCircleFilled(
+			pos2d,
+			radius,
+			isSelect ? 0xAF000000u : (isHover ? 0x9A000000u : 0x70000000u),
+			64
+		);
+
+		if (isSelect)
+			drawList.AddCircle(pos2d, radius, 0xFF000000, 64, 2f);
+
+		ImGui.SetCursorPos((pos2d - size / 2).AddX(0.75f));
+		Icons.DrawIcon(display.Icon, display.Color);
+
+		return isHover;
+	}
+
+	// ItemSelect
+
+	private class ItemSelect {
+		public readonly SceneObject Item;
+		public readonly Vector3 ScreenPos;
+		public readonly int SortPriority;
+
+		public string Name => this.Item.Name;
+
+		public bool IsHovered;
+
+		public ItemSelect(SceneObject item, Vector3 screenPos) {
+			this.Item = item;
+			this.ScreenPos = screenPos;
+			if (item is ISortPriority prio)
+				this.SortPriority = prio.SortPriority;
+		}
+	}
+}
---
+++ Ktisis/Scene/Impl/ISortPriority.cs
@@ -0,0 +1,5 @@
+namespace Ktisis.Scene.Impl;
+
+public interface ISortPriority {
+	public int SortPriority { get; set; }
+}
