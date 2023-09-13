e7db00d0e54dd58d156882d9545cfea7e5833aa2
chirp <72366111+chirpxiv@users.noreply.github.com>
2023-09-13 19:55:31 +0100

    Move `IDummy` impl: `BoneGroup` -> `ArmatureGroup`

--- Ktisis/Scene/Objects/Skeleton/BoneGroup.cs
+++ Ktisis/Scene/Objects/Skeleton/BoneGroup.cs
@@ -1,14 +1,9 @@
-using System.Linq;
-using System.Numerics;
-
-using Ktisis.Scene.Impl;
-using Ktisis.Common.Utility;
 using Ktisis.Data.Config.Bones;
 using Ktisis.Data.Config.Display;
 
 namespace Ktisis.Scene.Objects.Skeleton; 
 
-public class BoneGroup : ArmatureGroup, IDummy {
+public class BoneGroup : ArmatureGroup {
 	// Properties
 
 	public override ItemType ItemType => ItemType.BoneGroup;
@@ -33,53 +28,4 @@ public class BoneGroup : ArmatureGroup, IDummy {
 	// Stale check
 
 	public bool IsStale() => this.Children.Count == 0;
-	
-	// IDummy
-
-	public Transform Transform { get; set; } = new();
-
-	private Transform MakeTransform() {
-		var transforms = GetIndividualBones()
-			.Select(bone => bone.GetTransform())
-			.Where(trans => trans != null)
-			.Cast<Transform>()
-			.ToList();
-
-		var result = new Transform();
-
-		var count = transforms.Count;
-		if (count == 0) return result;
-
-		Quaternion rot;
-		if (this.GetCommonParent()?.GetTransform() is Transform pTrans) {
-			rot = pTrans.Rotation;
-		} else {
-			var weight = 1f / count;
-			rot = transforms
-				.Select(t => t.Rotation)
-				.Aggregate((a, b) => a * Quaternion.Slerp(Quaternion.Identity, b, weight));
-		}
-		
-		result = transforms.Aggregate(result, (a, b) => {
-			a.Position += b.Position;
-			a.Scale += b.Scale;
-			return a;
-		});
-        
-        result.Position /= count;
-		result.Rotation = Quaternion.Normalize(rot);
-		result.Scale /= count;
-		
-		return result;
-	}
-
-	public void CalcTransform()
-		=> this.Transform = MakeTransform();
-
-	public Transform GetTransform() {
-		var calc = MakeTransform();
-		if (Vector3.Distance(calc.Position, this.Transform.Position) > 0.1f)
-			this.Transform = calc;
-		return this.Transform;
-	}
 }
