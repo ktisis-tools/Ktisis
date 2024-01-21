cfd531162c5c0f2bd941f01360528eb338df432e
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-01-21 01:14:38 +0000

    Implement human.cmp parser

---
+++ Ktisis/Data/Files/Chara/CharaCommonColors.cs
@@ -0,0 +1,9 @@
+namespace Ktisis.Data.Files.Chara;
+
+public struct CommonColors {
+	public uint[] EyeColors;
+	public uint[] HighlightColors;
+	public uint[] LipColors;
+	public uint[] RaceFeatureColors;
+	public uint[] FacepaintColors;
+}
---
+++ Ktisis/Data/Files/Chara/CharaTribeColors.cs
@@ -0,0 +1,6 @@
+namespace Ktisis.Data.Files.Chara;
+
+public struct TribeColors {
+	public uint[] SkinColors;
+	public uint[] HairColors;
+}
