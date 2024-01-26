05f7c2ff6df6eadba6141d81d8001e7bc72062ec
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-01-26 22:28:26 +0000

    Reimplement keybind for gizmo visibility toggle

---
+++ Ktisis/Actions/Handlers/Gizmo/GizmoToggleAction.cs
@@ -0,0 +1,27 @@
+﻿using Dalamud.Game.ClientState.Keys;
+
+using Ktisis.Actions.Binds;
+using Ktisis.Actions.Types;
+using Ktisis.Core.Types;
+using Ktisis.Data.Config.Actions;
+
+namespace Ktisis.Actions.Handlers.Gizmo;
+
+[Action("Gizmo_Toggle")]
+public class GizmoToggleAction(IPluginContext ctx) : KeyAction(ctx) {
+	public override KeybindInfo BindInfo { get; } = new() {
+		Trigger = KeybindTrigger.OnDown,
+		Default = new ActionKeybind {
+			Enabled = true,
+			Combo = new KeyCombo(VirtualKey.G, VirtualKey.CONTROL)
+		}
+	};
+
+	public override bool CanInvoke() => this.Context.Editor != null;
+
+	public override bool Invoke() {
+		if (!this.CanInvoke()) return false;
+		this.Context.Config.File.Gizmo.Visible ^= true;
+		return true;
+	}
+}
