3adfca32668bc4710318bec0e6e3d549cf4f3bbb
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-01-26 23:51:08 +0000

    Reimplement keybind for gizmo mode toggle

---
+++ Ktisis/Actions/Handlers/Gizmo/GizmoModeAction.cs
@@ -0,0 +1,28 @@
+﻿using Dalamud.Game.ClientState.Keys;
+
+using Ktisis.Actions.Binds;
+using Ktisis.Actions.Types;
+using Ktisis.Core.Types;
+using Ktisis.Data.Config.Actions;
+using Ktisis.ImGuizmo;
+
+namespace Ktisis.Actions.Handlers.Gizmo;
+
+[Action("Gizmo_ToggleMode")]
+public class GizmoModeAction(IPluginContext ctx) : KeyAction(ctx) {
+	public override KeybindInfo BindInfo { get; } = new() {
+		Trigger = KeybindTrigger.OnDown,
+		Default = new ActionKeybind {
+			Enabled = true,
+			Combo = new KeyCombo(VirtualKey.X, VirtualKey.CONTROL)
+		}
+	};
+	
+	public override bool Invoke() {
+		if (this.Context.Editor == null || this.Context.Editor.Selection.Count == 0)
+			return false;
+		// ReSharper disable once BitwiseOperatorOnEnumWithoutFlags
+		this.Context.Config.File.Gizmo.Mode ^= Mode.World;
+		return true;
+	}
+}
