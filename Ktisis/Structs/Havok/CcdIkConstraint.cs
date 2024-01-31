5fb1e0c59fce948e46d57d418d6fd4c48f2b9ae8
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-01-31 00:46:11 +0000

    Upload CcdIkConstraint.cs

---
+++ Ktisis/Structs/Havok/CcdIkConstraint.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace Ktisis.Structs.Havok;
+
+[StructLayout(LayoutKind.Explicit)]
+public struct CcdIkConstraint {
+	// The start bone in this chain
+	[FieldOffset(0)] public short m_startBone;
+
+	// The end bone in this chain
+	[FieldOffset(2)] public short m_endBone;
+
+	// The target position for the end bone, in model space
+	[FieldOffset(16)] public Vector4 m_targetMS;
+}
