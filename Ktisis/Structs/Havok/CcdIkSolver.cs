b4fb647c073ac9a460b79488b43974eaa4dba1a5
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-01-31 00:48:31 +0000

    Upload CcdIkSolver.cs

---
+++ Ktisis/Structs/Havok/CcdIkSolver.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+using FFXIVClientStructs.Havok;
+
+namespace Ktisis.Structs.Havok;
+
+[StructLayout(LayoutKind.Explicit)]
+public struct CcdIkSolver {
+	[FieldOffset(0)] public hkReferencedObject hkRefObject;
+	
+	// The number of iterations of the IK solver
+	[FieldOffset(16)] public int m_iterations;
+	
+	// The gain factor to use in the IK solver
+	[FieldOffset(20)] public float m_gain;
+}
