191d5444cc3e6cb9fd3f3eb5afda8c0bb5b160f6
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-07-07 00:10:40 +0100

    correct drawdata customize offset

--- Ktisis/Structs/Actor/ActorDrawData.cs
+++ Ktisis/Structs/Actor/ActorDrawData.cs
@@ -8,7 +8,7 @@ namespace Ktisis.Structs.Actor {
 		[FieldOffset(0x0F0)] public Weapon Prop;
 		
 		[FieldOffset(0x160)] public Equipment Equipment;
-		[FieldOffset(0x188)] public Customize Customize;
+		[FieldOffset(0x1B0)] public Customize Customize;
 
 		[FieldOffset(0x1D0)] public ushort Glasses;
 	}
