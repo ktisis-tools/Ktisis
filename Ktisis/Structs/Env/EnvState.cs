fb8391a92a2d6325c9a423c112a9ba8dd74985eb
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-07-10 11:11:43 +0100

    update EnvState

--- Ktisis/Structs/Env/EnvState.cs
+++ Ktisis/Structs/Env/EnvState.cs
@@ -4,16 +4,16 @@ using Ktisis.Structs.Env.Weather;
 
 namespace Ktisis.Structs.Env;
 
-[StructLayout(LayoutKind.Explicit, Size = 0x258)]
+[StructLayout(LayoutKind.Explicit, Size = 0x2F8)]
 public struct EnvState {
 	[FieldOffset(0x008)] public uint SkyId;
 	
 	[FieldOffset(0x020)] public EnvLighting Lighting;
-	[FieldOffset(0x094)] public EnvStars Stars;
-	[FieldOffset(0x0BC)] public EnvFog Fog;
+	[FieldOffset(0x098)] public EnvStars Stars;
+	[FieldOffset(0x0C0)] public EnvFog Fog;
 	
-	[FieldOffset(0x104)] public EnvClouds Clouds;
-	[FieldOffset(0x12C)] public EnvRain Rain;
-	[FieldOffset(0x160)] public EnvDust Dust;
-	[FieldOffset(0x194)] public EnvWind Wind;
+	[FieldOffset(0x148)] public EnvClouds Clouds;
+	[FieldOffset(0x170)] public EnvRain Rain;
+	[FieldOffset(0x1A4)] public EnvDust Dust;
+	[FieldOffset(0x1D8)] public EnvWind Wind;
 }
