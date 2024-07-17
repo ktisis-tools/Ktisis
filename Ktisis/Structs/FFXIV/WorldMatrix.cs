3f8c0f28a652b60ccda711ad0131c68113b8f315
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-07-17 12:35:19 +0100

    dynamic resolution fix

--- Ktisis/Structs/FFXIV/WorldMatrix.cs
+++ Ktisis/Structs/FFXIV/WorldMatrix.cs
@@ -22,18 +22,16 @@ namespace Ktisis.Structs.FFXIV {
 		public bool WorldToScreenDepth(Vector3 v, out Vector3 pos2d) {
 			var m = Matrix;
 
-			var windowPos = ImGuiHelpers.MainViewport.Pos;
+			var x = (m.M11 * v.X) + (m.M21 * v.Y) + (m.M31 * v.Z) + m.M41;
+			var y = (m.M12 * v.X) + (m.M22 * v.Y) + (m.M32 * v.Z) + m.M42;
+			var w = (m.M14 * v.X) + (m.M24 * v.Y) + (m.M34 * v.Z) + m.M44;
 
-			float x = (m.M11 * v.X) + (m.M21 * v.Y) + (m.M31 * v.Z) + m.M41;
-			float y = (m.M12 * v.X) + (m.M22 * v.Y) + (m.M32 * v.Z) + m.M42;
-			float w = (m.M14 * v.X) + (m.M24 * v.Y) + (m.M34 * v.Z) + m.M44;
-
-			float camX = (Width / 2f);
-			float camY = (Height / 2f);
-
+			var view = ImGuiHelpers.MainViewport;
+			var camX = (view.Size.X / 2f);
+			var camY = (view.Size.Y / 2f);
 			pos2d = new Vector3(
-				camX + (camX * x / w) + windowPos.X,
-				camY - (camY * y / w) + windowPos.Y,
+				camX + (camX * x / w) + view.Pos.X,
+				camY - (camY * y / w) + view.Pos.Y,
 				w
 			);
 
