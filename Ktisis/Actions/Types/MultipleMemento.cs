13d29508fbe47ca8d9db43a80f19b41161470f7d
Jade Macho <ade@0x0a.de>
2024-08-05 12:20:05 +0200

    Add "Anchor group positions" for selected bone imports

---
+++ Ktisis/Actions/Types/MultipleMemento.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using Ktisis.Actions.Types;
+using Ktisis.Editor.Posing.Types;
+
+namespace Ktisis.Editor.Posing.Data;
+
+public class MultipleMemento(IReadOnlyList<IMemento?> mementos) : IMemento {
+	public IReadOnlyList<IMemento?> Mementos => mementos;
+
+	public void Restore() {
+		for (int i = mementos.Count - 1; i >= 0; i--) {
+			mementos[i]?.Restore();
+		}
+	}
+		
+	public void Apply() {
+		for (int i = 0; i < mementos.Count; i++) {
+			mementos[i]?.Apply();
+		}
+	}
+}
