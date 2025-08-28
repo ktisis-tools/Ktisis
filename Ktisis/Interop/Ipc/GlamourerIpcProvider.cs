3a1e8aca853b2bb0976e0d60738f7a4013151eba
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2025-08-28 11:11:37 -0500

    add state revert

--- Ktisis/Interop/Ipc/GlamourerIpcProvider.cs
+++ Ktisis/Interop/Ipc/GlamourerIpcProvider.cs
@@ -9,12 +9,14 @@ namespace Ktisis.Interop.Ipc;
 public class GlamourerIpcProvider {
 	private readonly ApplyState _applyState;
 	private readonly GetState _getState;
+	private readonly RevertState _revertState;
 	
 	public GlamourerIpcProvider(
 		IDalamudPluginInterface dpi
 	) {
 		this._applyState = new ApplyState(dpi);
 		this._getState = new GetState(dpi);
+		this._revertState = new RevertState(dpi);
 	}
 
 	public void ApplyState(string state, int index) {
@@ -23,5 +25,9 @@ public class GlamourerIpcProvider {
 
 	public void GetState(int index) {
 		this._getState.Invoke(index);
+	}
+
+	public void RevertState(int index) {
+		this._revertState.Invoke(index);
 	}
 }
