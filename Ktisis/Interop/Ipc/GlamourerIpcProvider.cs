aa09fed851d2a3db1a38eb5e46d4a42fca814fcb
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2025-08-28 10:55:32 -0500

    add glamourer GetState

--- Ktisis/Interop/Ipc/GlamourerIpcProvider.cs
+++ Ktisis/Interop/Ipc/GlamourerIpcProvider.cs
@@ -8,14 +8,20 @@ namespace Ktisis.Interop.Ipc;
 
 public class GlamourerIpcProvider {
 	private readonly ApplyState _applyState;
+	private readonly GetState _getState;
 	
 	public GlamourerIpcProvider(
 		IDalamudPluginInterface dpi
 	) {
 		this._applyState = new ApplyState(dpi);
+		this._getState = new GetState(dpi);
 	}
 
 	public void ApplyState(string state, int index) {
 		this._applyState.Invoke(state, index);
+	}
+
+	public void GetState(int index) {
+		this._getState.Invoke(index);
 	}
 }
