914fe6f15436bf7e0d6cebca4966dcde6c2313e3
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2025-08-30 13:12:27 -0500

    stub workcam keybinds

---
+++ Ktisis/Actions/Handlers/Camera/WorkcamFastAction.cs
@@ -0,0 +1,28 @@
+using Dalamud.Game.ClientState.Keys;
+
+using Ktisis.Actions.Attributes;
+using Ktisis.Actions.Binds;
+using Ktisis.Actions.Types;
+using Ktisis.Core.Types;
+using Ktisis.Data.Config.Actions;
+
+namespace Ktisis.Actions.Handlers.Camera;
+
+[Action("Camera_Work_Fast")]
+public class WorkcamFastAction(IPluginContext ctx) : KeyAction(ctx) {
+	public override KeybindInfo BindInfo { get; } = new() {
+		Trigger = KeybindTrigger.OnDown,
+		Default = new ActionKeybind {
+			Enabled = true,
+			Combo = new KeyCombo(VirtualKey.NO_KEY)
+		}
+	};
+
+	public override bool CanInvoke() => this.Context.Editor != null;
+	
+	public override bool Invoke() {
+		if (!this.CanInvoke()) return false;
+		this.Context.Editor!.Cameras.ToggleWorkCameraMode();
+		return true;
+	}
+}
