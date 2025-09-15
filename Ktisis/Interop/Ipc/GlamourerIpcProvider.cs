85cb221e6110865055f1009112d8f8483b817f4e
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2025-09-15 11:06:28 -0500

    add ipcs for designlist and design application

--- Ktisis/Interop/Ipc/GlamourerIpcProvider.cs
+++ Ktisis/Interop/Ipc/GlamourerIpcProvider.cs
@@ -2,11 +2,14 @@
 
 using Dalamud.Plugin;
 
+using Glamourer.Api.Enums;
 using Glamourer.Api.IpcSubscribers;
 
 namespace Ktisis.Interop.Ipc;
 
 public class GlamourerIpcProvider {
+	private readonly GetDesignList _getDesignList;
+	private readonly ApplyDesign _applyDesign;
 	private readonly ApplyState _applyState;
 	private readonly ApplyStateName _applyStateName;
 	private readonly GetState _getState;
@@ -16,11 +19,26 @@ public class GlamourerIpcProvider {
 	public GlamourerIpcProvider(
 		IDalamudPluginInterface dpi
 	) {
+		this._getDesignList = new GetDesignList(dpi);
+		this._applyDesign = new ApplyDesign(dpi);
 		this._applyState = new ApplyState(dpi);
 		this._applyStateName = new ApplyStateName(dpi);
 		this._getState = new GetState(dpi);
 		this._revertState = new RevertState(dpi);
 		this._revertStateName = new RevertStateName(dpi);
+	}
+
+	public Dictionary<Guid, string> GetDesignList() => this._getDesignList.Invoke();
+
+	public void ApplyDesignToObject(IGameObject gameObject, Guid designId) {
+		Ktisis.Log.Verbose($"Setting design for '{gameObject.Name}' ({gameObject.ObjectIndex}) to '{designId}'");
+
+		var (result, prev) = this._applyDesign.Invoke(designId, index);
+		var success = result == GlamourerApiEc.Success;
+		if (!success)
+			Ktisis.Log.Warning($"Glamourer design application failed with return code: {result}");
+
+		return success;
 	}
 
 	public void ApplyState(string state, int index) {
