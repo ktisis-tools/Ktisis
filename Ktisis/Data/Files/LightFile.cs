a57a0e6ea1a859dc9e40f84c339d9ef69a26cfb1
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2025-09-26 14:41:05 -0500

    stub ktcamera and ktlight

---
+++ Ktisis/Data/Files/LightFile.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+using Ktisis.Structs.Lights;
+using Ktisis.Structs.Common;
+
+using FFXIVClientStructs.FFXIV.Client.Graphics;
+
+namespace Ktisis.Data.Files;
+
+public class LightFile : JsonFile {
+	public new string FileExtension { get; set; } = ".ktlight";
+	public new string TypeName { get; set; } = "Ktisis Light";
+	public const int CurrentVersion = 1;
+	public new int FileVersion { get; set; } = CurrentVersion;
+
+	public LightFlags Flags { get; set; }
+	public LightType LightType { get; set; }
+	public unsafe Transform* Transform { get; set; } = null;
+	public ColorHDR Color { get; set; } = new ColorHDR();
+	public float ShadowNear { get; set; } = 0;
+	public float ShadowFar { get; set; } = 0;
+	public FalloffType FalloffType { get; set; }
+	public Vector2 AreaAngle { get; set; } = Vector2.Zero;
+	public float Falloff { get; set; } = 0;
+	public float LightAngle { get; set; } = 0;
+	public float FalloffAngle { get; set; } = 0;
+	public float Range { get; set; } = 0;
+	public float CharaShadowRange { get; set; } = 0;
+}
