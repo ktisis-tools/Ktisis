2675d78715d2e5152da783bb1e576b0708ba7fec
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2025-09-26 15:27:50 -0500

    unstub lightconverter

--- Ktisis/Editor/Lights/EntityLightConverter.cs
+++ Ktisis/Editor/Lights/EntityLightConverter.cs
@@ -8,16 +8,55 @@ using Ktisis.Scene.Entities.World;
 namespace Ktisis.Editor.Lights;
 
 public class EntityLightConverter {
-    private readonly LightEntity _light;
+    private LightEntity _light;
 
     public EntityLightConverter(LightEntity light) => this._light = light;
 
-    public void Apply(LightFile file) {
-        
+    public unsafe void Apply(LightFile file) {
+		var sceneLight = this._light.GetObject();
+		var light = sceneLight != null ? sceneLight->RenderLight : null;
+		if (light == null) return;
+
+        this._light.Flags |= LightEntityFlags.Update;
+
+        light->Flags = file.Flags;
+        light->LightType = file.LightType;
+        // light->Transform = file.Transform;
+        light->Color = file.Color;
+        light->ShadowNear = file.ShadowNear;
+        light->ShadowFar = file.ShadowFar;
+        light->FalloffType = file.FalloffType;
+        light->AreaAngle = file.AreaAngle;
+        light->Falloff = file.Falloff;
+        light->LightAngle = file.LightAngle;
+        light->FalloffAngle = file.FalloffAngle;
+        light->Range = file.Range;
+        light->CharaShadowRange = file.CharaShadowRange;
     }
 
     public LightFile Save() {
 		var file = new LightFile { Nickname = this._light.Name };
+        this.Write(file);
         return file;
+    }
+
+    private unsafe void Write(LightFile file) {
+		var sceneLight = this._light.GetObject();
+		var light = sceneLight != null ? sceneLight->RenderLight : null;
+		if (light == null) return;
+
+        file.Flags = light->Flags;
+        file.LightType = light->LightType;
+        // file.Transform = light->Transform; TODO: local and world transform saving - scenelight vs renderlight transform?
+        file.Color = light->Color;
+        file.ShadowNear = light->ShadowNear;
+        file.ShadowFar = light->ShadowFar;
+        file.FalloffType = light->FalloffType;
+        file.AreaAngle = light->AreaAngle;
+        file.Falloff = light->Falloff;
+        file.LightAngle = light->LightAngle;
+        file.FalloffAngle = light->FalloffAngle;
+        file.Range = light->Range;
+        file.CharaShadowRange = light->CharaShadowRange;
     }
 }
