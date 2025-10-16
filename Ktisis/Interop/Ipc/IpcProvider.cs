333e6f6c7077cab49737e2af4df4d89bb507a799
Cazzar <Cazzar@users.noreply.github.com>
2025-10-17 01:57:17 +1100

    Add IPC support for bone matrix manipulation and selection

--- Ktisis/Interop/Ipc/IpcProvider.cs
+++ Ktisis/Interop/Ipc/IpcProvider.cs
@@ -1,10 +1,14 @@
-﻿using System.Threading.Tasks;
-using Dalamud.Game.ClientState.Objects.Types;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Ipc;
 using Ktisis.Core.Attributes;
 using Ktisis.Data.Files;
 using Ktisis.Editor.Context;
+using Ktisis.Scene.Entities.Game;
+using Ktisis.Scene.Entities.Skeleton;
 using Ktisis.Scene.Modules.Actors;
 using Newtonsoft.Json;
 
@@ -18,7 +22,10 @@ public class IpcProvider(ContextManager ctxManager, IDalamudPluginInterface dpi)
     private ICallGateProvider<bool> IpcIsPosing { get; } = dpi.GetIpcProvider<bool>("Ktisis.IsPosing");
     private ICallGateProvider<uint, string, Task<bool>> IpcLoadPose { get; } = dpi.GetIpcProvider<uint, string, Task<bool>>("Ktisis.LoadPose");
     private ICallGateProvider<uint, Task<string?>> IpcSavePose { get; } = dpi.GetIpcProvider<uint, Task<string?>>("Ktisis.SavePose");
-   
+    private ICallGateProvider<uint, string, Matrix4x4, Task<bool>> IpcSetMatrix { get; } = dpi.GetIpcProvider<uint, string, Matrix4x4, Task<bool>>("Ktisis.SetMatrix");
+    private ICallGateProvider<uint, string, Task<Matrix4x4?>> IpcGetMatrix { get; } = dpi.GetIpcProvider<uint, string, Task<Matrix4x4?>>("Ktisis.GetMatrix");
+    private ICallGateProvider<Task<Dictionary<int, HashSet<string>>>> IpcSelectedBones { get; } = dpi.GetIpcProvider<Task<Dictionary<int, HashSet<string>>>>("Ktisis.SelectedBones");
+
     private (int, int) GetVersion() => (1, 0);
 
     private bool RefreshActors()
@@ -57,6 +64,48 @@ public class IpcProvider(ContextManager ctxManager, IDalamudPluginInterface dpi)
         return JsonConvert.SerializeObject(file);
     }
 
+    private async Task<Dictionary<int, HashSet<string>>> SelectedBones()
+    {
+        var sceneChildren = ctxManager.Current?.Scene?.Children
+            .OfType<ActorEntity>()
+            .ToList();
+        
+        if (sceneChildren is null || sceneChildren.Count == 0)
+            return new ();
+
+        var ret = new Dictionary<int, HashSet<string>>();
+        
+        foreach (var actor in sceneChildren)
+        {
+            if (!actor.IsValid || actor.Pose is null)
+                continue;
+
+            ret[actor.Actor.ObjectIndex] = actor.Children.OfType<BoneNode>().Where(s => s.IsSelected).Select(s => s.Name).ToHashSet();
+        }
+
+        return ret;
+    }
+
+    private async Task<Matrix4x4?> GetMatrix(uint index, string boneName)
+    {
+        var actor = ctxManager.Current?.Scene?.GetEntityForIndex(index);
+
+        var bone = actor?.Pose?.Children.OfType<BoneNode>().FirstOrDefault(b => b.Name == boneName);
+        return bone?.GetMatrix();
+    }
+    
+    private async Task<bool> SetMatrix(uint index, string boneName, Matrix4x4 matrix)
+    {
+        var actor = ctxManager.Current?.Scene?.GetEntityForIndex(index);
+
+        var bone = actor?.Pose?.Children.OfType<BoneNode>().FirstOrDefault(b => b.Name == boneName);
+        if (bone is null)
+            return false;
+
+        bone.SetMatrix(matrix);
+        return true;
+    }
+    
     public void RegisterIpc()
     {
         IpcVersion.RegisterFunc(GetVersion);
@@ -64,5 +113,8 @@ public class IpcProvider(ContextManager ctxManager, IDalamudPluginInterface dpi)
         IpcIsPosing.RegisterFunc(IsActive);
         IpcLoadPose.RegisterFunc(LoadPose);
         IpcSavePose.RegisterFunc(SavePose);
+        IpcSetMatrix.RegisterFunc(SetMatrix);
+        IpcGetMatrix.RegisterFunc(GetMatrix);
+        IpcSelectedBones.RegisterFunc(SelectedBones);
     }
 }
