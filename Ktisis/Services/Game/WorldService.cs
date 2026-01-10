b1c2e488eaa3054e1a852bb42cd3e4ccbcc6a6f7
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2026-01-10 00:48:03 -0600

    init worldservice

---
+++ Ktisis/Services/Game/WorldService.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
+
+using Ktisis.Common.Utility;
+using Ktisis.Core.Attributes;
+using Ktisis.Structs.Objects;
+
+namespace Ktisis.Services.Game;
+
+[Singleton]
+public class WorldService : IDisposable {
+	private readonly GPoseService _gpose;
+	private bool _init;
+
+	public readonly List<WorldObject> Objects = [];
+	public readonly Dictionary<nint, Transform> Transforms = new();
+
+	public WorldService(
+		GPoseService gpose
+	) {
+		this._gpose = gpose;
+	}
+
+	private void OnGPoseEvent(object sender, bool active) {
+		if (!this._init) return;
+		this.Clean();
+		if (active) this.BuildWorld();
+	}
+
+	public bool Touch(WorldObject obj) {
+		if (!this.Objects.Contains(obj)) {
+			Ktisis.Log.Error($"Object {obj} is not present in world!");
+			return false;
+		}
+		if (this.Transforms.ContainsKey(obj.Address)) {
+			Ktisis.Log.Error($"Object {obj} has already been added to scene!");
+			return false;
+		}
+
+		this.Transforms.Add(obj.Address, obj.Transform);
+		return true;
+	}
+
+	public void Refresh() {
+		this.Clean();
+		this.BuildWorld();
+	}
+
+	private void BuildWorld() {
+		Ktisis.Log.Debug($"starting worldobject fetch...");
+		this.Objects.AddRange(this.RecurseWorld().Where(obj => obj.ObjectType is ObjectType.BgObject));
+		Ktisis.Log.Debug($"finished! {this.Objects.Count} bgobjects found");
+		this._init = true;
+	}
+
+	private IEnumerable<WorldObject> RecurseWorld() {
+		var worldObj = this.GetWorld();
+		if (worldObj == null) yield break;
+		yield return worldObj.Value;
+
+		foreach (var sibling in worldObj.Value.GetSiblings()) {
+			yield return sibling;
+			foreach (var child in this.RecurseChildren(sibling))
+				yield return child;
+		}
+
+		foreach (var child in this.RecurseChildren(worldObj.Value))
+			yield return child;
+	}
+
+	private IEnumerable<WorldObject> RecurseChildren(WorldObject worldObj) {
+		foreach (var child in worldObj.GetChildren()) {
+			yield return child;
+			foreach (var reChild in this.RecurseChildren(child))
+				yield return reChild;
+		}
+	}
+
+	private unsafe WorldObject? GetWorld() {
+		var world = World.Instance();
+		if (world == null) return null;
+		return new WorldObject(&world->Object);
+	}
+
+	private void Clean() {
+		this.Objects.Clear();
+		this.Transforms.Clear();
+		this._init = false;
+	}
+
+	public void Dispose() {
+		this.Clean();
+		this._gpose.StateChanged -= this.OnGPoseEvent;
+	}
+}
---
+++ Ktisis/Structs/Objects/WorldObject.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+
+using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
+using FFXIVClientStructs.Interop;
+
+using Ktisis.Common.Utility;
+
+using Object = FFXIVClientStructs.FFXIV.Client.Graphics.Scene.Object;
+
+namespace Ktisis.Structs.Objects;
+
+// https://github.com/ktisis-tools/ffxiv-pathfinder/blob/main/Pathfinder/Objects/Data/WorldObject.cs
+
+public struct WorldObject : IEquatable<WorldObject> {
+	private readonly Pointer<Object> Pointer;
+	public nint Address;
+
+	public unsafe WorldObject(Pointer<Object> ptr) {
+		this.Pointer = ptr;
+		this.Address = (nint)ptr.Value;
+	}
+
+	public unsafe WorldObject(Object* ptr) {
+		this.Pointer = ptr;
+		this.Address = (nint)ptr;
+	}
+
+	// Data wrappers
+
+	public unsafe ObjectType ObjectType => this.Pointer.Value->GetObjectType();
+
+	public unsafe Transform Transform => new (
+		this.Pointer.Value->Position,
+		this.Pointer.Value->Rotation,
+		this.Pointer.Value->Scale
+	);
+
+	// Enumerate children & siblings
+
+	private unsafe WorldObject? GetFirstChild() {
+		if (this.Pointer.Value == null) return null;
+		var ptr = this.Pointer.Value;
+		var child = ptr->ChildObject;
+		return child != null && child != ptr ? new WorldObject(child) : null;
+	}
+
+	private unsafe WorldObject? NextSibling() {
+		if (this.Pointer.Value == null) return null;
+		var ptr = this.Pointer.Value;
+		var sibling = ptr->NextSiblingObject;
+		if (sibling == null || sibling == ptr)
+			return null;
+		return new WorldObject(sibling);
+	}
+
+	public IEnumerable<WorldObject> GetChildren() {
+		var child = this.GetFirstChild();
+		if (child == null) yield break;
+		yield return child.Value;
+
+		var firstSibling = child.Value.NextSibling();
+		var sibling = firstSibling;
+		while (sibling != null && sibling.Value.Address != this.Address && sibling.Value.Address != child.Value.Address) {
+			yield return sibling.Value;
+			sibling = sibling.Value.NextSibling();
+			if (sibling?.Address == firstSibling?.Address)
+				break;
+		}
+	}
+
+	public IEnumerable<WorldObject> GetSiblings() {
+		var sibling = this.NextSibling();
+
+		while (sibling != null && sibling.Value.Address != this.Address) {
+			yield return sibling.Value;
+			sibling = sibling.Value.NextSibling();
+		}
+	}
+
+	public bool Equals(WorldObject other) => this.Address == other.Address;
+}
