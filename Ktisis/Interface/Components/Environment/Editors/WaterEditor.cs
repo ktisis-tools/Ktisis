7c87378e08df75d0d7586afe3c65e4cdbad19de4
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2026-01-13 22:10:01 -0600

    unfreeze water for next session when leaving gpose

--- Ktisis/Interface/Components/Environment/Editors/WaterEditor.cs
+++ Ktisis/Interface/Components/Environment/Editors/WaterEditor.cs
@@ -2,23 +2,40 @@ using System;
 
 using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.Utility.Raii;
+using Dalamud.Plugin.Services;
 
 using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
 
 using Ktisis.Core.Attributes;
 using Ktisis.Scene.Modules;
+using Ktisis.Services.Game;
 using Ktisis.Structs.Env;
 
 namespace Ktisis.Interface.Components.Environment.Editors;
 
 [Singleton]
 public class WaterEditor : EditorBase, IDisposable {
-	public bool Frozen = false;
+	private GPoseService _gpose;
+	private readonly IFramework _framework;
 	private unsafe WaterRendererEx* _renderer;
 
-	public unsafe WaterEditor() {
+	public bool Frozen = false;
+
+	public unsafe WaterEditor(
+		IFramework framework,
+		GPoseService gpose
+	) {
+		this._framework = framework;
+		this._gpose = gpose;
+
 		var manager = Manager.Instance();
 		this._renderer = (WaterRendererEx*)&manager->WaterRenderer;
+
+		// delay registration by 1 tick to prevent enumeration error as this may be initialized during GPoseService's action
+		this._framework.RunOnTick(() => {
+			this._gpose.StateChanged += this.OnGPoseEvent;
+			this._gpose.Subscribe();
+		}, delayTicks:1);
 	}
 
 	public override string Name { get; } = "Water";
@@ -49,8 +66,13 @@ public class WaterEditor : EditorBase, IDisposable {
 			this._renderer->Unk4 = scrub4;
 	}
 
+	private void OnGPoseEvent(object sender, bool active) {
+		if (!active) this.Frozen = false;
+	}
+
 	public unsafe void Dispose() {
 		this._renderer = null;
 		this.Frozen = false;
+		this._gpose.StateChanged -= this.OnGPoseEvent;
 	}
 }
