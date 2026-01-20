03a52f6f00db74f14187630219d74f0f010b1e83
Karou <leaguehatesme@gmail.com>
2026-01-20 17:44:45 -0500

    Bunny please help

--- Ktisis/Interface/KTK/PreviewNode.cs
+++ Ktisis/Interface/KTK/PreviewNode.cs
@@ -1,14 +1,27 @@
+using System.Linq;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
+using Dalamud.Game;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 
 using KamiToolKit.Classes;
 using KamiToolKit.Enums;
 using KamiToolKit.Nodes;
 using KamiToolKit.Overlay;
 using KamiToolKit.Extensions;
+
+using Ktisis.Common.Extensions;
+using Ktisis.Services.Plugin;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Ktisis.Interface.KTK;
 
@@ -20,8 +33,17 @@ public unsafe class PreviewNode : OverlayNode {
 	private readonly ImageNode Image;
 	private readonly RenderTargetManager* _renderTargetManager;
 	private readonly AgentTryon* _agentTryon;
+	private readonly IFramework _framework;
+	private readonly IObjectTable _objectTable;
+	private uint _counter;
 
-	public PreviewNode() {
+	public PreviewNode(
+		IFramework framework,
+		IObjectTable objectTable) {
+		this._framework = framework;
+		this._objectTable =  objectTable;
+		this._counter = 1;
+		
 		this._renderTargetManager = RenderTargetManager.Instance();
 		this._agentTryon = AgentTryon.Instance();
 		this.Image = new ImageNode() {
@@ -31,8 +53,16 @@ public unsafe class PreviewNode : OverlayNode {
 		};
 		var part = this.Image.AddPart(new Part());
 		part->LoadTexture(this._renderTargetManager->CharaViewTextures[2]);
+		this._agentTryon->CharaView.Initialize(null, 2, 0);
 		this._renderTargetManager->CharaViewTextures[2].Value->IncRef();
-		this._agentTryon->CharaView.VirtualTable->Initialize(&this._agentTryon->CharaView, &this._agentTryon->AgentInterface, 0, 0);
+		this._framework.Update += OnFramework;
 		this.Image.AttachNode(this);
+	}
+	
+
+	void OnFramework(IFramework framework) => this.Update();
+	public void Update() {
+		this._agentTryon->CharaView.Render(this._counter++);
+		
 	}
 }
