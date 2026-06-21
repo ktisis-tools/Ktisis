30755a0b8500a0a84932fd8d346a4d4122a4b070
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2026-06-20 22:07:08 -0500

    text updates for v2 migration dialog

--- Ktisis/Legacy/Interface/V2MigratorWindow.cs
+++ Ktisis/Legacy/Interface/V2MigratorWindow.cs
@@ -30,6 +30,8 @@ public class V2MigratorWindow {
 	public void DrawIntro() {
 		ImGui.Text(this.Locale.Translate("migrator.mainWindow.v2.main_Desc"));
 		ImGui.Spacing();
+		ImGui.Text(this.Locale.Translate("migrator.mainWindow.v2.migration_desc"));
+		ImGui.Spacing();
 		ImGui.AlignTextToFramePadding();
 		ImGui.Text(this.Locale.Translate("migrator.mainWindow.v2.wiki"));
 		ImGui.SameLine();
@@ -41,7 +43,7 @@ public class V2MigratorWindow {
 		ImGui.Text(this.Locale.Translate("migrator.v2.editor.header"));
 		ImGui.Separator();
 		DialogHelpers.BuildDialog(ref this._cfg.File.Categories.ShowNsfwBones, false, this.Locale.Translate("migrator.v2.editor.nsfwTooltip"), this.Locale.Translate("migrator.v2.editor.nsfw"), string.Empty);
-		DialogHelpers.BuildDialog(ref this._cfg.File.Editor.IncognitoPlayerNames, false, this.Locale.Translate("migrator.v2.editor.incognitoTooltip"), this.Locale.Translate("migrator.v2.editor.incognito"), string.Empty);
+		DialogHelpers.BuildDialog(ref this._cfg.File.Editor.IncognitoPlayerNames, false, this.Locale.Translate("migrator.v2.editor.incognitoTooltip"), this.Locale.Translate("migrator.v2.editor.incognito"), this.Locale.Translate("migrator.v2.editor.incognitoSub"));
 		DialogHelpers.BuildDialog(ref this._cfg.File.Editor.UseToolbar, true, string.Empty, this.Locale.Translate("migrator.v2.editor.toolbar"), this.Locale.Translate("migrator.v2.editor.toolbarSub"));
 	}
 
@@ -49,8 +51,9 @@ public class V2MigratorWindow {
 		using var _ = ImRaii.PushId("Input");
 		ImGui.Text(this.Locale.Translate("migrator.v2.input.header"));
 		ImGui.Separator();
+		ImGui.Text(this.Locale.Translate("migrator.v2.input.detail"));
+		ImGui.Spacing();
 		DialogHelpers.BuildDialog(ref this._cfg.File.Keybinds.Enabled, true, string.Empty, this.Locale.Translate("migrator.v2.input.keybinds"), string.Empty);
-		DialogHelpers.BuildDialog(ref this._cfg.File.Gizmo.AllowAxisFlip, true, string.Empty, this.Locale.Translate("migrator.v2.input.allowAxisFlip"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.Keybinds.BlockTargetLeftClick, false, string.Empty, this.Locale.Translate("migrator.v2.input.leftClick"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.Keybinds.BlockTargetRightClick, false, string.Empty, this.Locale.Translate("migrator.v2.input.rightClick"), string.Empty);
 	}
@@ -59,9 +62,12 @@ public class V2MigratorWindow {
 		using var _ = ImRaii.PushId("Overlay");
 		ImGui.Text(this.Locale.Translate("migrator.v2.overlay.header"));
 		ImGui.Separator();
+		ImGui.Text(this.Locale.Translate("migrator.v2.overlay.detail"));
+		ImGui.Spacing();
 		DialogHelpers.BuildDialog(ref this._cfg.File.Overlay.DrawLines, true, string.Empty, this.Locale.Translate("migrator.v2.overlay.drawLines"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.Overlay.DrawLinesGizmo, true, string.Empty, this.Locale.Translate("migrator.v2.overlay.drawLinesGizmo"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.Overlay.DrawDotsGizmo, true, string.Empty, this.Locale.Translate("migrator.v2.overlay.drawDotsGizmo"), string.Empty);
+		DialogHelpers.BuildDialog(ref this._cfg.File.Gizmo.AllowAxisFlip, true, this.Locale.Translate("migrator.v2.overlay.allowAxisFlipTip"), this.Locale.Translate("migrator.v2.overlay.allowAxisFlip"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.Overlay.LineThickness, 2.0f, string.Empty, this.Locale.Translate("migrator.v2.overlay.lineThickness"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.Overlay.LineOpacity, 0.95f, string.Empty, this.Locale.Translate("migrator.v2.overlay.lineOpacity"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.Overlay.LineOpacityUsing, 0.15f, string.Empty, this.Locale.Translate("migrator.v2.overlay.lineOpacityUsing"), string.Empty);
@@ -72,8 +78,10 @@ public class V2MigratorWindow {
 		using var _ = ImRaii.PushId("Auto save");
 		ImGui.Text(this.Locale.Translate("migrator.v2.autosave.header"));
 		ImGui.Separator();
+		ImGui.Text(this.Locale.Translate("migrator.v2.autosave.detail"));
+		ImGui.Spacing();
 		DialogHelpers.BuildDialog(ref this._cfg.File.AutoSave.Enabled, true, string.Empty, this.Locale.Translate("migrator.v2.autosave.enabled"), string.Empty);
-		DialogHelpers.BuildDialog(ref this._cfg.File.AutoSave.Interval, 60, string.Empty, this.Locale.Translate("migrator.v2.autosave.interval"), string.Empty);
+		DialogHelpers.BuildDialog(ref this._cfg.File.AutoSave.Interval, 60, this.Locale.Translate("migrator.v2.autosave.intervalTip"), this.Locale.Translate("migrator.v2.autosave.interval"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.AutoSave.Count, 5, string.Empty, this.Locale.Translate("migrator.v2.autosave.numToSave"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.AutoSave.ClearOnExit, false, string.Empty, this.Locale.Translate("migrator.v2.autosave.clearOnExit"), string.Empty);
 		DialogHelpers.BuildDialog(ref this._cfg.File.AutoSave.OnDisconnect, true, string.Empty, this.Locale.Translate("migrator.v2.autosave.autoOnDC"), string.Empty);
