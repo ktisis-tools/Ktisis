42b619b171ce0e821b1f0a053179c7f61b17d8a9
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2026-07-11 10:38:45 -0500

    remove gubal fetcher and bnpc json from resources

--- fetch_bnpcs.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set source="https://raw.githubusercontent.com/ffxiv-teamcraft/ffxiv-teamcraft/staging/libs/data/src/lib/json/gubal-bnpcs-index.json"
-set dest="./Ktisis/Data/Library/bnpc-index.json"
-curl %source% -o %dest%
