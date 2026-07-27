bec81fa7630427db5a4ccb985e15ee614109169e
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2026-07-27 10:28:44 -0500

    move OverworldActorPopup to a searchable PopupList

--- Ktisis/Interface/Editor/Popup/OverworldActorPopup.cs
+++ Ktisis/Interface/Editor/Popup/OverworldActorPopup.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 using Dalamud.Game.ClientState.Objects.Types;
 
 using Dalamud.Bindings.ImGui;
 
-using GLib.Lists;
+using GLib.Popups;
 
 using Ktisis.Common.Extensions;
 using Ktisis.Editor.Context.Types;
@@ -17,7 +18,7 @@ namespace Ktisis.Interface.Editor.Popup;
 public class OverworldActorPopup : KtisisPopup {
 	private readonly ActorService _actors;
 	private readonly IEditorContext _ctx;
-	private readonly ListBox<IGameObject> _list;
+	private readonly PopupList<IGameObject> _list;
 
 	public OverworldActorPopup(
 		ActorService actors,
@@ -25,10 +26,10 @@ public class OverworldActorPopup : KtisisPopup {
 	) : base("##OverworldActorPopup") {
 		this._actors = actors;
 		this._ctx = ctx;
-		this._list = new ListBox<IGameObject>(
+		this._list = new PopupList<IGameObject>(
 			"##OverworldActorList",
 			DrawActorName
-		);
+		).WithSearch(MatchQuery);
 	}
 
 	protected override void OnDraw() {
@@ -36,8 +37,10 @@ public class OverworldActorPopup : KtisisPopup {
 			this.Close();
 			return;
 		}
+		if (!this._list.IsOpen)
+			this._list.Open();
 		
-		var actors = this._actors.GetOverworldActors().ToList();
+		var actors = this._actors.GetOverworldActors().OrderBy(a => a.CurrentDistance).ToList();
 		if (this._list.Draw(actors, out var selected) && selected!.IsEnabled())
 			this.AddActor(selected!);
 	}
@@ -48,5 +51,8 @@ public class OverworldActorPopup : KtisisPopup {
 	}
 	
 	private bool DrawActorName(IGameObject actor, bool isFocus)
-		=> ImGui.Selectable(actor.GetNameOrFallback(this._ctx), isFocus);
+		=> ImGui.Selectable(actor.GetNameOrFallback(this._ctx) + (actor.CurrentDistance > 0 ? $" ({actor.CurrentDistance:#y})" : ""), isFocus);
+
+	private static bool MatchQuery(IGameObject actor, string query)
+		=> actor.Name.ToString().Contains(query, StringComparison.OrdinalIgnoreCase);
 }
